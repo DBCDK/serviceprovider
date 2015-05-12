e8f2d0b32addf1293215e2511717b0ce2c4502c3
Martin Møller <mmj@dbc.dk>
2015-05-12 11:01:06 +0200

    debugging syslog

--- logger.js
+++ logger.js
@@ -1,21 +1,26 @@
 'use strict';
 
 let winston = require('winston');
+require('winston-syslog').Syslog; // eslint-disable-line no-unused-expressions
 
 let logger = new (winston.Logger)({
   transports: [
-    new winston.transports.Console({json: true})
+    new (winston.transports.Console)({json: true})
   ]
 });
 
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development') { // eslint-disable-line no-process-env
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development' || process.env.NODE_ENV === undefined) { // eslint-disable-line no-process-env
   logger.log('info', 'adding syslog');
   require('winston-syslog').Syslog; // eslint-disable-line no-unused-expressions
+
   logger.add(winston.transports.Syslog, {
     protocol: 'udp4',
     app_name: 'palles_gavebod'
   });
   logger.setLevels(winston.config.syslog.levels);
 }
+
+logger.log('info', 'HEST');
+logger.info('hest');
 
 module.exports = logger;
