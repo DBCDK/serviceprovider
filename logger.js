cfb3364805789b5691fffdf70b94c8d34154ff83
Martin Møller <mmj@dbc.dk>
2015-05-12 10:06:18 +0200

    debugging syslog

--- logger.js
+++ logger.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'developme
   logger.log('info', 'adding syslog');
   require('winston-syslog').Syslog; // eslint-disable-line no-unused-expressions
   logger.add(winston.transports.Syslog, {
-    protocol: 'udp4',
+    protocol: 'udp6',
     path: '/var/log/syslog',
     app_name: 'palles_gavebod'
   });
