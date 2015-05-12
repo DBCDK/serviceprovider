fc229f4830adbcd4eb6c6748a529c40f1f18fdb4
Martin Møller <mmj@dbc.dk>
2015-05-12 11:50:27 +0200

    debugging syslog

--- logger.js
+++ logger.js
@@ -4,24 +4,23 @@ let winston = require('winston');
 
 let logger = new (winston.Logger)({
   transports: [
-    new (winston.transports.Console)({json: true, level: 'silly'})
+    new (winston.transports.Console)({json: true, timestamp: true, level: 'alert'})
   ]
 });
 
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development' || process.env.NODE_ENV === undefined) { // eslint-disable-line
+logger.setLevels(winston.config.syslog.levels);
+
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development') { // eslint-disable-line
   logger.log('info', 'adding syslog');
   require('winston-syslog').Syslog; // eslint-disable-line no-unused-expressions
 
   logger.add(winston.transports.Syslog, {
     protocol: 'udp4',
     app_name: 'palles gavebod',
-    tag: 'palles gavebod',
-    json: true
+    json: true,
+    timestamp: true,
+    level: 'alert'
   });
-  logger.setLevels(winston.config.syslog.levels);
 }
-
-logger.log('info', 'HEST');
-logger.info('hest');
 
 module.exports = logger;
