437784d0af5d1bef32196ad24ad8f6b625f39eaf
Martin Møller <mmj@dbc.dk>
2015-05-12 11:18:38 +0200

    debugging syslog

--- logger.js
+++ logger.js
@@ -1,26 +1,25 @@
 'use strict';
 
 let winston = require('winston');
-require('winston-syslog').Syslog; // eslint-disable-line no-unused-expressions
 
 let logger = new (winston.Logger)({
   transports: [
-    new (winston.transports.Console)({json: true}),
-    new (winston.transports.Syslog)({protocol: 'udp4', app_name: 'palles_gavebod'})
+    new (winston.transports.Console)({json: true, level: 'silly'})
   ]
 });
-/*
- if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development' || process.env.NODE_ENV === undefined) { // eslint-disable-line
- logger.log('info', 'adding syslog');
- require('winston-syslog').Syslog; // eslint-disable-line no-unused-expressions
 
- logger.add(winston.transports.Syslog, {
- protocol: 'udp4',
- app_name: 'palles_gavebod'
- });
- logger.setLevels(winston.config.syslog.levels);
- }
- */
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development' || process.env.NODE_ENV === undefined) { // eslint-disable-line
+  logger.log('info', 'adding syslog');
+  require('winston-syslog').Syslog; // eslint-disable-line no-unused-expressions
+
+  logger.add(winston.transports.Syslog, {
+    protocol: 'udp4',
+    app_name: 'palles gavebod',
+    tag: 'palles gavebod',
+    json: true
+  });
+  logger.setLevels(winston.config.syslog.levels);
+}
 
 logger.log('info', 'HEST');
 logger.info('hest');
