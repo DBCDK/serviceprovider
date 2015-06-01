47056d70edf938ce1a9ff59fa69af039daf685a2
Søren Vibjerg <vibjerg@gmail.com>
2015-06-01 16:19:50 +0200

    extract css

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,17 @@
 
 var webpack = require('webpack');
 var path = require('path');
+var extractTextPlugin = require('extract-text-webpack-plugin');
+
 
 var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
 var noErrorsPlugin = new webpack.NoErrorsPlugin();
+var extractCss = new extractTextPlugin('style.css');
 
 module.exports = {
   entry: {
-    frontpage: './src/components/frontpage/index.js'
+    frontpage: './src/components/frontpage/index.js',
+    querysearch: './src/components/querySearch/index.js'
   },
   output: {
     path: path.join(__dirname, 'public/js'),
@@ -20,11 +24,20 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: extractTextPlugin.extract(
+          // activate source maps via loader query
+          'css?sourceMap!' +
+          'sass?sourceMap'
+        )
       }
     ]
   },
   plugins: [
     commonsPlugin,
+    extractCss,
     noErrorsPlugin
   ]
 };
