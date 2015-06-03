b332a3a12d26729bc4a4cb7b35d9070cb9add97d
Martin Møller <mmj@dbc.dk>
2015-06-03 13:50:17 +0200

    - reorganized file - added a bit of docs - add dbc-node-serviceprovider

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 'use strict';
 require('babel/register');
 
+// loading config etc.
 const newrelic = require('newrelic');
+const config = require('../config');
+const version = require('../package.json').version;
+
+// loading libraries
 const express = require('express');
-const path = require('path');
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io');
+const socket = io(server);
+const path = require('path');
 const logger = require('./logger');
-const version = require('../package.json').version;
+
+// settings up our provider
+var serviceProvider = require('dbc-node-serviceprovider');
+serviceProvider.init(config, socket);
 
 // Port config
 app.set('port', process.env.PORT || 8080); // eslint-disable-line no-process-env
@@ -16,13 +26,17 @@ app.set('port', process.env.PORT || 8080); // eslint-disable-line no-process-env
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// setting paths
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../static')));
 
+// setting local vars that should be availbe to our template engine
 app.locals.newrelic = newrelic;
 app.locals.version = version;
 app.locals.env = process.env; // eslint-disable-line no-process-env
 
+
+// setting basic routes -- should be moved elsewhere in the future
 app.get('/', function(req, res) {
   res.render('logo');
 });
@@ -35,7 +49,7 @@ app.get('/querysearch', function(req, res) {
   res.render('querysearch');
 });
 
-// startup server
+// starting server
 server.listen(app.get('port'), function() {
   logger.info('Server listening on ' + app.get('port'));
   logger.info({message: 'Versions: ', data: process.versions});
