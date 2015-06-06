d4752d6cdabf7f7fa572e0529e7bab421df0f4a3
Vibjerg <vibjerg@gmail.com>
2015-06-06 11:33:21 +0200

    enable querystrings

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ var extractCss = new extractTextPlugin('style.css');
 
 module.exports = {
   entry: {
-    //querysearch: './src/components/querySearch/index.js',
+    querysearch: './src/components/querySearch/index.js',
     logo: './src/components/logo/index.js',
     autocomplete: './src/components/autocomplete/index.js'
   },
