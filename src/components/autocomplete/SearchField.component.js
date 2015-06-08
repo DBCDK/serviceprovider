63f4e4b7708d940c86314781655c32adf9e9100b
Martin Møller <mmj@dbc.dk>
2015-06-08 12:35:49 +0200

    Passing AutoComplete component as an argument

--- src/components/autocomplete/SearchField.component.js
+++ src/components/autocomplete/SearchField.component.js
@@ -1,31 +1,60 @@
 'use strict';
 import React from 'react';
 import Socket from 'socket.io-client';
+import Reflux from 'reflux';
+
 const socket = Socket.connect();
 
-var SearchField = React.createClass({
-  componentDidMount() {
-    socket.on('getPopSuggestionsResponse', (data) => {
-      console.log(data); // eslint-disable-line no-console
-    });
-    socket.emit('getPopSuggestionsRequest', {index: 'hest', fields: ['a', 'b', 'c']});
+const actions = Reflux.createActions(
+  ['textfieldUpdated']
+);
+
+const store = Reflux.createStore({
+  listenables: [actions],
+
+  _store: {
+    text: '',
+    data: {}
+  },
+
+  onTextfieldUpdated(value) {
+    console.log(value);
+    this.trigger(this._store);
   },
 
   getInitialState() {
-    return {text: (this.props.initialValue) ? this.props.initialValue : ''};
-  },
+    return this._store;
+  }
+});
+
+/*
+ socket.on('getPopSuggestionsResponse', (data) => {
+ console.log(data); // eslint-disable-line no-console
+ });
+ socket.emit('getPopSuggestionsRequest', {index: 'hest', fields: ['a', 'b', 'c']});
+ */
+
+var SearchField = React.createClass({
+  mixins: [Reflux.connect(store)],
 
   render() {
+    const AutoComplete = this.props.autocomplete;
     return (
       <div>
         <input type='text' placeholder='Søg og du skal finde' value={this.state.text} onChange={this._onchange}/>
+        <AutoComplete />
       </div>
     );
   },
 
   _onchange(e) {
     const txt = e.target.value;
+    actions.textfieldUpdated(txt);
     this.setState({text: txt});
+  },
+
+  propTypes: {
+    autocomplete: React.PropTypes.func.isRequired
   }
 });
 
