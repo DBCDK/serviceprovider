3d29629343b5790acea271f83d694085e81702fa
Søren Vibjerg <vibjerg@gmail.com>
2015-06-08 10:00:12 +0200

    docs

--- src/components/querySearch/querySearch.client.js
+++ src/components/querySearch/querySearch.client.js
@@ -6,5 +6,9 @@ import '../../styles/app.scss';
 
 /**
  * Client side rendering of the QueryString component
+ *
+ * THIS IS A DUMMY IMPLEMENTATION
+ * This component should implemented in the main search field
+ *
  */
 React.render(<QueryString query={window.QUERYSTRING_PROPS || []}/>, document.getElementById('search'));
