24e033e848ab768b6606d41892e5064d23333bd1
Søren Vibjerg <vibjerg@gmail.com>
2015-06-08 14:19:34 +0200

    add actions from SearchField

--- src/components/querySearch/Search.react.js
+++ src/components/querySearch/Search.react.js
@@ -2,19 +2,41 @@
 import React from 'react';
 import {SearchField as QueryString, FilterGuide} from 'dbc-react-querystring';
 
+
+
+function Query () {
+
+
+}
+
+
 /**
  * Search field wrapper component
  */
 const Search = React.createClass({
+  getInitialState(){
+    return {
+      query: this.props.query || [],
+      elements: this.props.elements || []
+    };
+  },
   propTypes: {
 
   },
+  addElementToQuery(element, event){
+    let query = this.state.query;
+    query.push(element.label);
+    this.updateQuery(query);
+  },
+  updateQuery(query) {
+    this.setState({query});
+  },
   render() {
-    const {elements, query} = this.props;
+    const {elements, query} = this.state;
     return (
       <div className='search'>
-        <QueryString query={query}/>
-        <FilterGuide elements={elements}/>
+        <QueryString query={query} update={this.updateQuery} />
+        <FilterGuide elements={elements} select={this.addElementToQuery}/>
       </div>
     );
   }
