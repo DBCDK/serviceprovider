1502520af47030e58559c78fcd8e6d2d928303b5
Søren Vibjerg <vibjerg@gmail.com>
2015-06-16 09:44:32 +0200

    remove debug message

--- src/components/autocomplete/SearchField.component.js
+++ src/components/autocomplete/SearchField.component.js
@@ -73,7 +73,7 @@ const store = Reflux.createStore({
   },
 
   onTextfieldUpdated(value) {
-    console.log('onTextfieldUpdated');
+    // console.log('onTextfieldUpdated');
     socket.emit('getPopSuggestionsRequest', value);
   },
 
