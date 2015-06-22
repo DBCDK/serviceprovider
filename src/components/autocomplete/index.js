351aedd008a0b2c2d96bc6b1ec4e1c55826260a5
Martin Møller <mmj@dbc.dk>
2015-06-22 12:55:51 +0200

    comment added

--- src/components/autocomplete/index.js
+++ src/components/autocomplete/index.js
@@ -1,4 +1,11 @@
 'use strict';
+
+/**
+ * @file
+ * The purpose of this file is to examplify how the AutoComplete component is
+ * used and implemented. This part of the application serves no purpose for
+ * endusers and could be deleted without losing any enduser faced functionality.
+ */
 import React from 'react';
 import AutoComplete from './AutoComplete.component.js';
 
