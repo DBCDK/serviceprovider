12a90b96de7c79304ad527d0c2cfcf7fa595e795
Søren Vibjerg <vibjerg@gmail.com>
2015-06-22 10:53:51 +0200

    Tabs for switching between search result and recommendations

---
+++ src/components/Search/SearchTabs.component.js
@@ -0,0 +1,32 @@
+'use strict';
+import React from 'react';
+
+var SearchTabs = React.createClass({
+  propTypes: {
+    buttons: React.PropTypes.array.isRequired,
+    selected: React.PropTypes.string
+  },
+
+  render() {
+    const selected = this.props.selected || this.props.buttons[0];
+    const buttons = this.props.buttons.map(button => {
+      let classes = ['button', 'secondary'];
+      if (button === selected) {
+        classes.push('active');
+      }
+      return (
+        <li>
+          <a onClick={this.props.update.bind(null, button)} href="#" className={classes.join(' ')}>{button}</a>
+        </li>
+      );
+    });
+
+    return (
+      <ul className="button-group even-2">
+        {buttons}
+      </ul>
+    );
+  }
+});
+
+export default SearchTabs;
