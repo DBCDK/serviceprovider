ab1ad16d2f9185195f48b373ecc049d76cc39856
Matias Rasmussen <rasmussen.matias@gmail.com>
2015-08-06 17:06:52 +0200

    Simple login component created

---
+++ src/components/Login/Login.component.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @file
+ * Login component displays the login form.
+ */
+
+import React from 'react';
+
+
+const Login = React.createClass({
+
+  componentDidMount: function () {
+  },
+
+  handleSubmit: function () {
+  },
+
+  render: function () {
+    return (
+      <div>
+        <h2>Log ind</h2>
+        <input type='text' ref='email'>email</input>
+        <input type='password' ref='password'>password</input>
+        <input type='button' value='Log ind' onClick={this.handleSubmit}></input>
+
+        <div className='row'>
+          <div className='small-6 columns'><a href='/signup'>Ny Profil?</a></div>
+          <div className='small-6 columns'><a href='/password-reset'>Glemt password?</a></div>
+        </div>
+      </div>
+    );
+  }
+});
+
+export default Login;
