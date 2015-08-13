061bdbc4484c705ef7e80bd3a11b88d7ac08d7a0
Matias Rasmussen <rasmussen.matias@gmail.com>
2015-08-13 10:36:47 +0200

    Minimal styling for login component

--- src/components/Login/Login.component.js
+++ src/components/Login/Login.component.js
@@ -20,12 +20,14 @@ const Login = React.createClass({
     return (
       <div>
         <h2>Log ind</h2>
-        <input type='text' ref='email'>email</input>
-        <input type='password' ref='password'>password</input>
-        <input type='button' value='Log ind' onClick={this.handleSubmit}></input>
+        <label>Email</label>
+        <input type='text' ref='email'></input>
+        <label>Password</label>
+        <input type='password' ref='password'></input>
+        <input className='button' type='button' value='Log ind' onClick={this.handleSubmit}></input>
 
         <div className='row'>
-          <div className='small-6 columns'><a href='/signup'>Ny Profil?</a></div>
+          <div className='small-6 columns'><a href='/signup'>Ny Bruger?</a></div>
           <div className='small-6 columns'><a href='/resetpassword'>Glemt password?</a></div>
         </div>
       </div>
