d0b1175c13343d162161d8dbbb1421641c378678
Martin Møller <mmj@dbc.dk>
2015-08-26 12:24:53 +0200

    minor codeformatting

--- src/components/Profile/Profile.component.js
+++ src/components/Profile/Profile.component.js
@@ -13,38 +13,40 @@ import ProfileSocialSummary from './ProfileSocialSummary.component.js';
 
 import ProfileStore from '../../stores/Profile.store.js';
 
-
 const Profile = React.createClass({
 
   displayName: function() {
     return 'ReactProfile';
   },
 
-  getInitialState: function () {
+  getInitialState: function() {
     return {profile: ProfileStore.getProfile()};
   },
 
-  componentDidMount: function () {
+  componentDidMount: function() {
     ProfileStore.listen(this.updateProfile);
   },
 
-  updateProfile: function (profile) {
+  updateProfile: function(profile) {
     this.setState({profile: profile});
   },
 
-  render: function () {
-    let profile = this.state.profile;
-    let editable = profile.editEnabled;
-    let groupsCount = profile.groupsCount;
-    let followingCount = profile.followingCount;
-    let followersCount = profile.followersCount;
+  render: function() {
+    const profile = this.state.profile;
+    const editable = profile.editEnabled;
+    const groupsCount = profile.groupsCount;
+    const followingCount = profile.followingCount;
+    const followersCount = profile.followersCount;
     return (
       <div>
-        <ProfileHeader editable={editable} ref='header'/>
+        <ProfileHeader editable={editable} ref='header' />
         <ProfileImage editable={editable} url={profile.imageUrl} />
-        <ProfileAttribute editable={editable} name='Name' type='string' value={profile.name}/>
-        <ProfileSocialSummary followerCount={followersCount} followingCount={followingCount}
-                              groupsCount={groupsCount} />
+        <ProfileAttribute editable={editable} name='Name' type='string' value={profile.name} />
+        <ProfileSocialSummary
+          followerCount={followersCount}
+          followingCount={followingCount}
+          groupsCount={groupsCount}
+          />
       </div>
     );
   }
