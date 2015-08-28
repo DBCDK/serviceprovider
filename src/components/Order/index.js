d4ee016f0c425922d4dbc7825034cc1f2a951ce4
Rikke Lose <rikkelose@dhcp106.dbc.dk>
2015-08-28 15:05:13 +0200

    hopefully fix error

--- src/components/Order/index.js
+++ src/components/Order/index.js
@@ -9,7 +9,7 @@ import {rewriteCoverImageUrl} from '../../utils/CoverImage.util.js';
  *
  * If a querystring from the url exists it is added to the global window object, and should be passed to the client
  */
- 
+
 const image = <CoverImage pids={window.ORDER_PROPS.coverImageIds.split(',')} prefSize={'detail_500'} rewriteImgUrl={rewriteCoverImageUrl} />;
 
 React.render(<Order coverImage={image} order={window.ORDER_PROPS || {}} />, document.getElementById('order'));
