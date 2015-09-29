0384538cb7b1a7cd1ad935f5c420d4041b21d7fc
Matias Rasmussen <rasmussen.matias@gmail.com>
2015-09-29 14:23:18 +0200

    Added new Group component

---
+++ src/components/Group/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @file
+ * This file is the entrypoint for the Group component
+ */
+
+import React from 'react';
+import Group from './Group.component.js';
+
+React.render(<Group/>, document.getElementById('group'), null);
