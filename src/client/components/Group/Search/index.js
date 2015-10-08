80e85f3ef9e21ccde438428f0ecee270a11df5f7
Martin Møller <mmj@dbc.dk>
2015-10-08 14:42:27 +0200

    SSR Group

--- src/client/components/Group/Search/index.js
+++ src/client/components/Group/Search/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-import React from 'react';
+import ReactDOM from 'react-dom';
 import GroupSearchContainerComponent from './GroupSearchContainer.component.js';
 
-React.render(
+ReactDOM.render(
   <GroupSearchContainerComponent
     groupData={window.groupSearchProps.groupData || []}
     query={window.groupSearchProps.qObj || []} />,
