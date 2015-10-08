3149e3c76768f9cad290172cc952777bc00f5b72
Martin Møller <mmj@dbc.dk>
2015-10-08 14:42:58 +0200

    SSR Login

--- src/client/components/Login/index.js
+++ src/client/components/Login/index.js
@@ -5,7 +5,7 @@
  * This file is the entrypoint for the Profile component
  */
 
-import React from 'react';
+import ReactDOM from 'react-dom';
 import Login from './Login.component.js';
 
-React.render(<Login/>, document.getElementById('login'), null);
+ReactDOM.render(<Login/>, document.getElementById('login'), null);
