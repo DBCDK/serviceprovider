304b95a940e120983cc68cc43513cf8962cced9d
Matias Rasmussen <rasmussen.matias@gmail.com>
2015-10-13 14:30:08 +0200

    Time since created added to comments

--- src/client/components/Group/Post/Comment.component.js
+++ src/client/components/Group/Post/Comment.component.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import React, {PropTypes} from 'react';
+import {timeSince} from '../../../../utils/Time.util.js';
 
 class CommentComponent extends React.Component {
   constructor(props, context) {
@@ -8,13 +9,16 @@ class CommentComponent extends React.Component {
   }
 
   render() {
+    const timeSinceCreated = timeSince(this.props.timeCreated);
+
     return (
       <div className="row group--comment">
         <div className="large-1 medium-2 hide-for-small-only columns">
           <img className='group--member-image' src={this.props.ownerImageUrl}/>
         </div>
         <div className="large-11 medium-10 small-12 columns">
-          <p><strong>{this.props.ownerEmail}:</strong> {this.props.commentContent}</p>
+          <div><strong>{this.props.ownerEmail}:</strong> {this.props.commentContent}</div>
+          <span className='group--timesince'>{timeSinceCreated}</span>
         </div>
       </div>
     );
@@ -25,7 +29,8 @@ CommentComponent.displayName = 'Comment.component';
 CommentComponent.propTypes = {
   commentContent: PropTypes.string.isRequired,
   ownerEmail: PropTypes.string.isRequired,
-  ownerImageUrl: PropTypes.string.isRequired
+  ownerImageUrl: PropTypes.string.isRequired,
+  timeCreated: PropTypes.string.isRequired
 };
 
 export default CommentComponent;
