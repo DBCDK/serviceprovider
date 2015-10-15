b4a7df6d489ebc0c74fbe8ffd7ae4887206b1fee
Martin Møller <mmj@dbc.dk>
2015-10-15 10:10:03 +0200

    disabled helmet

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ import expressValidator from 'express-validator';
 import compression from 'compression';
 import expressSession from 'express-session';
 import RedisStore from 'connect-redis';
-import helmet from 'helmet';
+// import helmet from 'helmet';
 
 // loading routes
 import MainRoutes from './server/routes/main.routes.js';
@@ -48,16 +48,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // Helmet configuration
+/*
 app.use(helmet.xssFilter());
 app.use(helmet.frameguard());
 app.use(helmet.hsts({
   maxAge: 112200,
   includeSubdomains: true
 }));
-app.use(helmet.hidePoweredBy({setTo: 'Funkys Venner'}));
+app.use(helmet.hidePoweredBy({setTo: 'Funkys Venner!'}));
 app.use(helmet.ieNoOpen());
 app.use(helmet.noSniff());
-
+*/
 // Port config
 app.set('port', process.env.PORT || 8080); // eslint-disable-line no-process-env
 
@@ -87,6 +88,8 @@ app.locals.env = ENV;
 app.locals.version = version;
 app.locals.production = PRODUCTION;
 app.locals.title = config.palle.applicationTitle || '';
+app.locals.application = process.env.NODE_APPLICATION === 'ddbmobil' ? 'mobilsoeg' : 'pg'; // eslint-disable-line no-process-env
+
 
 // setup environments
 let redisConfig;
