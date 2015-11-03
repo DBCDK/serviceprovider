193bf848e3ddd61a8a21a077b8f06fa91c3addc9
Jonas Peter Hyatt <jph@dbc.dk>
2015-11-03 15:45:06 +0100

    us118: Recommendations på værker

---
+++ src/client/actions/Recommendations.actions.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import Reflux from 'reflux';
+import SocketClient from 'dbc-node-serviceprovider-socketclient';
+let socket = SocketClient('getRecommendations');
+
+const RecommendationActions = Reflux.createActions([
+  'getRecommendations',
+  'getRecommendationsResponse'
+]);
+
+RecommendationActions.getRecommendations.listen(socket.request);
+socket.response(RecommendationActions.getRecommendationsResponse);
+
+export default RecommendationActions;
