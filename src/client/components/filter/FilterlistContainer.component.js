54c197ed0afd87a826a65e62f55b903b5194d5e4
Matias Rasmussen <rasmussen.matias@gmail.com>
2015-11-20 15:52:52 +0100

    remove opensuggest filter buttons

--- src/client/components/filter/FilterlistContainer.component.js
+++ src/client/components/filter/FilterlistContainer.component.js
@@ -20,12 +20,11 @@ import MaterialTypeStore from '../../stores/MaterialType.store.js';
 export default React.createClass({
   displayName: 'FilterListContainer',
   mixins: [
-    Reflux.connect(FilterStore, 'filter'),
     Reflux.connect(MaterialTypeStore, 'categories')
   ],
 
   render() {
-    const filterGuide = this.state.filter.length && (<FilterGuide categories={this.state.categories.categories} elements={this.state.filter} select={QueryActions.add} />) || '';
+    const filterGuide = (<FilterGuide categories={this.state.categories.categories} elements={this.state.filter} select={QueryActions.add} />) || '';
     return (
       <div className='filterguide--wrapper' >
         {filterGuide}
