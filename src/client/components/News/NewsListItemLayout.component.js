cc10e8189c2301008896b34a01eb5cb26ed0ac6f
Matias Rasmussen <rasmussen.matias@gmail.com>
2015-12-17 13:07:28 +0100

    Fixed DDBContent image url

--- src/client/components/News/NewsListItemLayout.component.js
+++ src/client/components/News/NewsListItemLayout.component.js
@@ -19,7 +19,7 @@ export default class NewsListItemLayout extends React.Component {
           <div className="headline" >{this.props.title}</div>
           <div className="image" >
             {this.props.image &&
-            <img alt={this.props.title} className='news-item-image' src={`data:image/png;base64,${this.props.image}`} />
+            <img alt={this.props.title} className='news-item-image' src={`http://rest.filmstriben.dbc.inlead.dk/web/${this.props.image}`} />
             }
           </div>
           <div className="lead" >
