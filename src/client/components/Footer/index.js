243422667bead3d15733b9f50b39a6096f9aabb8
Matias Rasmussen <rasmussen.matias@gmail.com>
2015-12-21 15:59:10 +0100

    Added opening hours to footer ( and added footer )

---
+++ src/client/components/Footer/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @file
+ * This file is the entrypoint for the Footer component
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Footer from './Footer.component.js';
+
+ReactDOM.render(
+  <Footer/>, document.getElementById('footer'), null
+);
