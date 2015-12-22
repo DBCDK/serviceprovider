5f39c9122daa501b77ded838b73086faf9240d6c
Matias Rasmussen <rasmussen.matias@gmail.com>
2015-12-22 14:40:31 +0100

    Added time info to events on frontpage

--- src/client/components/Calendar/EventListItemLayout.component.js
+++ src/client/components/Calendar/EventListItemLayout.component.js
@@ -13,9 +13,29 @@ import React from 'react';
 
 export default class EventListItemLayout extends React.Component {
   render() {
+
+    const months = [
+      'Januar',
+      'Februar',
+      'Marts',
+      'April',
+      'Maj',
+      'Juni',
+      'Juli',
+      'August',
+      'September',
+      'Oktober',
+      'November',
+      'December',
+    ];
+    let datetimeParts = this.props.start.split(/[:\ \-]/);
+    const time = datetimeParts[3] + ":" + datetimeParts[4];
+    const date = datetimeParts[2] + ". " + months[Number(datetimeParts[1])+1];
+
     return (
       <div className={`news-item clearfix ${this.props.zebra}`} >
         <a className="" href={`${this.props.link}`} >
+          <div className="time">{date} kl. {time}</div>
           <div className="headline" >{this.props.title}</div>
           <div className="image" >
             {this.props.image &&
@@ -37,6 +57,7 @@ EventListItemLayout.propTypes = {
   body: React.PropTypes.string.isRequired,
   image: React.PropTypes.string,
   lead: React.PropTypes.string.isRequired,
+  start: React.PropTypes.string,
   link: React.PropTypes.string.isRequired,
   title: React.PropTypes.string.isRequired,
   zebra: React.PropTypes.string.isRequired
