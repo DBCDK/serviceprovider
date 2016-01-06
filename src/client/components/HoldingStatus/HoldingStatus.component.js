499392a007c9b2a0055a52ccc00af11dee994bda
Jonas Peter Hyatt <jph@dbc.dk>
2016-01-06 14:26:12 +0100

    sd-198: holdingstatus på knapper

--- src/client/components/HoldingStatus/HoldingStatus.component.js
+++ src/client/components/HoldingStatus/HoldingStatus.component.js
@@ -34,22 +34,37 @@ class HoldingStatusComponent extends React.Component {
   render() {
     let result = '';
     let classes = 'status ';
+    let colour = '';
     if (this.state.holdingStatus && this.props.accessType === 'physical') {
-      if (this.state.holdingStatus.now === true) {
+      if (this.state.holdingStatus.now) {
         result = 'Hjemme';
         classes += 'available-now';
+        colour = '#41C001'; // ALCOOL
       }
-      else if (this.state.holdingStatus.available === true) {
+      else if (this.state.holdingStatus.available) {
         const date = new Date(this.state.holdingStatus.expectedDelievery);
         const pickupDate = (this.state.holdingStatus.expectedDelievery ? date.getDate() + '/' + (date.getMonth() + 1) + '-' + date.getFullYear() : '');
         result = 'Hjemme den ' + pickupDate;
         classes += 'available-later';
+        colour = '#FB1'; // FBI
       }
-      else if (this.state.holdingStatus.available === false || this.state.holdingStatus === 'item_not_found') {
+      else if (!this.state.holdingStatus.available || this.state.holdingStatus === 'item_not_found') {
         result = 'Udlånes ikke';
         classes += 'not-available';
+        colour = 'F00'; // FOO
       }
     }
+
+    if (this.props.compact) {
+      return (
+        <span className={'holding-status holding-status--compact'}>
+          <svg className={'holding-status-svg-cicle'} xmlns="http://www.w3.org/2000/svg" version="1.1">
+            <circle cx="10" cy="10" r="5" stroke="#333" strokeWidth="1" fill={colour} />
+          </svg>
+        </span>
+      );
+    }
+
     return (
       <div className={'work-container--work--editions--holding-status'}>
         <div className={classes}>{result}</div>
@@ -61,6 +76,7 @@ class HoldingStatusComponent extends React.Component {
 HoldingStatusComponent.displayName = 'HoldingStatus.component';
 HoldingStatusComponent.propTypes = {
   accessType: PropTypes.string.isRequired,
+  compact: PropTypes.bool,
   identifier: PropTypes.string.isRequired
 };
 
