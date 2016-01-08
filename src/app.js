df7a3095c5f3260605904cac68418bafbeba39a3
Søren Vibjerg <vibjerg@gmail.com>
2016-01-08 14:18:17 +0100

    implement service client cache

--- src/app.js
+++ src/app.js
@@ -74,7 +74,7 @@ app.set('port', process.env.PORT || 8080); // eslint-disable-line no-process-env
 const EMAIL_REDIRECT = process.env.EMAIL_REDIRECT || 'localhost:' + app.get('port'); // eslint-disable-line no-process-env
 
 // Configure app variables
-app.set('serviceProvider', ServiceProviderSetup(config[process.env.CONFIG_NAME || DEFAULT_CONFIG_NAME].provider.services, logger, socket)); // eslint-disable-line no-process-env
+app.set('serviceProvider', ServiceProviderSetup(config[process.env.CONFIG_NAME || DEFAULT_CONFIG_NAME], logger, socket)); // eslint-disable-line no-process-env
 app.set('logger', logger);
 app.set('EMAIL_REDIRECT', EMAIL_REDIRECT);
 app.set('APPLICATION', APPLICATION);
