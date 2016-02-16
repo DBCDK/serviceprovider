39e17ed1d9d6986866572135edf619a9ebbc98d1
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-02-16 14:19:14 +0100

    started super simple api test

---
+++ apitest/apitest.sh
@@ -0,0 +1,13 @@
+rm test.result
+
+IFS=$'\n$'
+for line in `cat requests.lst`
+do
+  printf "\n\n$line \n" >> test.result
+  curl -H "Content-Type: application/json" \
+       -X POST \
+       -d "[`echo $line | sed -e 's/[^ ]*//'`]" \
+       "http://localhost:8080/api/`echo $line | sed -e s/' .*//'`" \
+       >> test.result
+done
+diff -u test.result test.expected
