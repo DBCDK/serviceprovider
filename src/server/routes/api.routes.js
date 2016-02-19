b32d22f77d233623711fab930dac36ea5204c422
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-02-19 15:10:00 +0100

    initial swagger-file, serve generated documentation as default, fixes #10

--- src/server/routes/api.routes.js
+++ src/server/routes/api.routes.js
@@ -7,8 +7,15 @@
 
 import express from 'express';
 
-export default express.Router().post(['/:event'], (req, res) => {
+export default express.Router().all(['/:event'], (req, res) => {
   const event = req.params.event;
+  if (event === 'swagger.json') {
+    require('fs').readFile(
+      __dirname + '/../../../api/swagger/swagger.yaml',
+      (err, result) => res.send(JSON.stringify(require('js-yaml').safeLoad(result))))
+    return;
+  }
+
   const query = req.body[0];
 
   // very long timeout
