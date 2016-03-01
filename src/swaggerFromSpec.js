303fffab35d5ccb4a6f82bdf62f552d911d947b1
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-03-01 20:14:11 +0100

    sample spec+swagger-content in progress

--- src/swaggerFromSpec.js
+++ src/swaggerFromSpec.js
@@ -11,17 +11,58 @@ export default function() {
         fs.readFile(path + 'description.md', 'utf-8', (err, result) => err ? reject(err) : resolve(result)))])
     .then(([spec, desc]) => {
       return {
+        'x-spec': spec,
         swagger: '2.0',
         info: {
           version: '0.0.1',
           title: 'DBC ServiceProvider',
           description: desc
         },
-        schemes: [
-          'https', 'wss'
-        ],
-        'x-spec': spec,
+        basePath: '/api',
+        schemes: ['https', 'wss'],
+        consumes: ['application/json'],
+        produces: ['application/json'],
         paths: {
+          '/foo': {
+            post: {
+              description: 'Some description',
+              parameters: [{
+                name: 'someparam',
+                in: 'body',
+                description: 'param desc',
+                required: true,
+                schema: {
+                  type: 'object',
+                  properties: {
+                    a: {
+                      type: 'integer',
+                      format: 'int32'
+                    },
+                    b: {
+                      type: 'string'
+                    }
+                  }
+                }
+              }],
+              responses: {
+                200: {
+                  description: 'result desc...',
+                  schema: {
+                    type: 'string'
+                  }
+                },
+                default: {
+                  description: 'error',
+                  schema: {
+                    type: 'array',
+                    items: {
+                      type: 'string'
+                    }
+                  }
+                }
+              }
+            }
+          }
         },
         definitions: {
         },
