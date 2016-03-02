b31a4f5e833d427fed34e6c1ab888d3a4f6b72ae
Christian Boesgaard <cbo@dbc.dk>
2016-03-02 08:29:55 +0100

    write multiple files

--- apitest/wiremock/gen.py
+++ apitest/wiremock/gen.py
@@ -5,12 +5,23 @@
 from __future__ import division
 import sys
 
-def wiremock_string( wiremockpath, host, port ):
+def wiremock_rec_string( wiremockpath, host, port ):
     #url="http:"
     return "java -jar %s --port %s --proxy-all=%s --record-mappings --verbose" % (wiremockpath, port, host)
 
+def wiremock_play_string( wiremockpath, host, port = None):
+    #url="http:"
+    return "java -jar %s --port %s --verbose" % (wiremockpath, port)
+
+
 wiremock = "/home/cbo/serviceprovider/trunk/apitest/wiremock/wiremock-1.57-standalone.jar"
+
+envfile = open('env.sh', 'w')
+playfile = open('play.sh', 'w')
+recordfile = open('record.sh', 'w')
+cleanfile = open('clean.sh', 'w')
 if __name__ == "__main__":
+    
     for line in sys.stdin:
         l = line.split()
         #print ">>>", l
@@ -25,10 +36,13 @@ if __name__ == "__main__":
         port = l[3] # 9900
         print "###",host, url, env, port, "###"
         dirname = env
-        print "mkdir -p", dirname
-        ws = wiremock_string(wiremock, host, port)
-        print "(cd %s && nohup %s > nohup.out)&" % (dirname,ws)
+        recordfile.write("mkdir -p %s\n" % (dirname))
+        cleanfile.write("rm -rf %s/__files %s/mappings %s/*.out\n" % (dirname,dirname,dirname))
+        wrs = wiremock_rec_string(wiremock, host, port)
+        recordfile.write("(cd %s && nohup %s > record.out)&\n" % (dirname,wrs))
+        wps = wiremock_play_string(wiremock, host, port)
+        playfile.write("(cd %s && nohup %s > play.out)&\n" % (dirname,wps))
         # export OPENAGENCY_SERVICE_URL='http://localhost:9901/2.24/?wsdl'
         # TODO: https fix!
         mockurl=host.replace(host, "http://localhost:"+port)
-        print "export %s='%s'" % (env,mockurl+url)
+        envfile.write("export %s='%s'\n" % (env,mockurl+url))
