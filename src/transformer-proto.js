397c8f9bddc9b5e38cb37e705afe7136f4beb025
Jesper Damkjaer <jda@dbc.dk>
2016-03-08 10:46:18 +0100

    Fixed lint of transformer-proto.js

--- src/transformer-proto.js
+++ src/transformer-proto.js
@@ -1,34 +1,29 @@
 'use strict';
 require('babel/register');
-const fs = require('fs');
-const poman = require('./provider/Provider.js');
+// const fs = require('fs');
+// const poman = require('./provider/Provider.js');
 const entsuggest = require('./services/EntitySuggest/client.js');
 
 
 var config = {
-    endpoint: 'http://xptest.dbc.dk/ms/entity-suggest/v1/'
+  endpoint: 'http://xptest.dbc.dk/ms/entity-suggest/v1/'
 };
 
 var entclient = entsuggest(config);
 
-var response = entclient.getSubjectSuggestions({'query': 'hest'});
+var response = entclient.getSubjectSuggestions({query: 'hest'});
 
-response.then(function(result){
-    console.log( 'promise fulfilled eller noget' );    
-    console.log( JSON.stringify(result.response.suggestions,null,4) );  
+response.then(function(result) {
+  console.log('promise fulfilled eller noget'); // eslint-disable-line
+  console.log(JSON.stringify(result.response.suggestions, null, 4)); // eslint-disable-line
 }, function(err) {
-    console.log(err); // Error: "It broke"
+  // Error: "It broke"
+  console.log(err); // eslint-disable-line
 });
 
-	    
 
-
-//
-
-//console.log( JSON.stringify(response.response.suggestions, null, 4));
-
-//fs.watch('/tmp/heynode.txt', function(){
+// console.log( JSON.stringify(response.response.suggestions, null, 4));
+// fs.watch('/tmp/heynode.txt', function(){
 //	     console.log('Holy smokes! Something happened!');
-//});
-
-//console.log("let's roll!");
+// });
+// console.log("let's roll!");
