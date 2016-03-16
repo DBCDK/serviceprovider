16bec7c4e61ca36350e9059cc6e6317f821a3dfe
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-03-16 09:51:38 +0100

    remove wallaby and webpack

--- tests.serviceprovider.webpack.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-let context = require.context('./src/transformers', true, /\.test\.js?$/);
-context.keys().forEach(context);
-
-// http://kentor.me/posts/testing-react-and-flux-applications-with-karma-and-webpack/ & https://github.com/kentor/react-flux-testing/blob/master/tests.webpack.js
