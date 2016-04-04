b46d124c5cf9048e1e79710fd01feab351dbd962
flashgordon <shm@dbc.dk>
2016-04-04 16:17:25 +0200

    added comments, and refactored each transformer to contain request, response and clientfunctions

--- src/provider/neoSuggest.js
+++ src/provider/neoSuggest.js
@@ -37,115 +37,147 @@ function changeKey(obj, fromKey, toKey) {
   return obj;
 }
 
-
-function subjectSuggestRequest(request, context) { // eslint-disable-line no-unused-vars
-  let clientRequest = {query: request.q, n: request.limit};
-  return clientRequest;
-}
-
-function subjectSuggestResponse(response, context) { // eslint-disable-line no-unused-vars
-  return response.response.suggestions.map((obj) => {
-    return {str: obj.suggestion};
-  });
-}
-
-function subjectSuggestFunction(context) {
-  let contextCopy = clone(context);
-  changeKey(contextCopy, 'entityEndpoint', 'endpoint');
-  let client = entitySuggest(contextCopy);
-  return (request, localContext) => { // eslint-disable-line no-unused-vars
-    return client.getSubjectSuggestions(request);
-  };
-}
-
+/**
+* Transformer for subjectSuggest endpoint.
+* This transformer calls the entitySuggest client and returns subject
+* suggestions
+*/
 export function subjectSuggestTransformer() {
+
+  function subjectSuggestRequest(request, context) { // eslint-disable-line no-unused-vars
+    let clientRequest = {query: request.q, n: request.limit};
+    return clientRequest;
+  }
+
+  function subjectSuggestResponse(response, context) { // eslint-disable-line no-unused-vars
+    return response.response.suggestions.map((obj) => {
+      return {str: obj.suggestion};
+    });
+  }
+
+  function subjectSuggestFunction(context) {
+    let contextCopy = clone(context);
+    changeKey(contextCopy, 'entityEndpoint', 'endpoint');
+    let client = entitySuggest(contextCopy);
+    return (request, localContext) => { // eslint-disable-line no-unused-vars
+      return client.getSubjectSuggestions(request);
+    };
+  }
+
   return genericTransformer(subjectSuggestRequest,
                             subjectSuggestResponse,
                             subjectSuggestFunction);
 }
 
 
-function creatorSuggestRequest(request, context) { // eslint-disable-line no-unused-vars
-  let clientRequest = {query: request.q, n: request.limit};
-  return clientRequest;
-}
-
-function creatorSuggestResponse(response, context) { // eslint-disable-line no-unused-vars
-  return response.response.suggestions.map((obj) => {
-    return {str: obj.suggestion};
-  });
-}
-
-function creatorSuggestFunction(context) {
-  let contextCopy = clone(context);
-  changeKey(contextCopy, 'entityEndpoint', 'endpoint');
-  let client = entitySuggest(contextCopy);
-  return (request, localContext) => { // eslint-disable-line no-unused-vars
-    return client.getCreatorSuggestions(request);
-  };
-}
-
+/**
+* Transformer for creatorSuggest endpoint.
+* This transformer calls the entitySuggest client and returns creator
+* suggestions
+*/
 export function creatorSuggestTransformer() {
+
+  function creatorSuggestRequest(request, context) { // eslint-disable-line no-unused-vars
+    let clientRequest = {query: request.q, n: request.limit};
+    return clientRequest;
+  }
+
+  function creatorSuggestResponse(response, context) { // eslint-disable-line no-unused-vars
+    return response.response.suggestions.map((obj) => {
+      return {str: obj.suggestion};
+    });
+  }
+
+  function creatorSuggestFunction(context) {
+    let contextCopy = clone(context);
+    changeKey(contextCopy, 'entityEndpoint', 'endpoint');
+    let client = entitySuggest(contextCopy);
+    return (request, localContext) => { // eslint-disable-line no-unused-vars
+      return client.getCreatorSuggestions(request);
+    };
+  }
+
   return genericTransformer(creatorSuggestRequest,
                             creatorSuggestResponse,
                             creatorSuggestFunction);
 }
 
 
-function librarySuggestRequest(request, context) { // eslint-disable-line no-unused-vars
-  let clientRequest = {query: request.q, n: request.limit};
-  return clientRequest;
-}
-
-
-function librarySuggestResponse(response, context) { // eslint-disable-line no-unused-vars
-  return response.response.suggestions.map((obj) => {
-    obj.suggestion.str = obj.suggestion.navn + ', ' + obj.suggestion.by;
-    return obj.suggestion;
-  });
-}
-
-function librarySuggestFunction(context) {
-  let contextCopy = clone(context);
-  changeKey(contextCopy, 'entityEndpoint', 'endpoint');
-  let client = entitySuggest(contextCopy);
-  return (request, localContext) => { // eslint-disable-line no-unused-vars
-    return client.getLibrarySuggestions(request);
-  };
-}
-
+/**
+* Transformer for librarySuggest endpoint.
+* This transformer calls the entitySuggest client and returns library
+* suggestions
+*/
 export function librarySuggestTransformer() {
+
+  function librarySuggestRequest(request, context) { // eslint-disable-line no-unused-vars
+    let clientRequest = {query: request.q, n: request.limit};
+    return clientRequest;
+  }
+
+
+  function librarySuggestResponse(response, context) { // eslint-disable-line no-unused-vars
+    return response.response.suggestions.map((obj) => {
+      obj.suggestion.str = obj.suggestion.navn + ', ' + obj.suggestion.by;
+      return obj.suggestion;
+    });
+  }
+
+  function librarySuggestFunction(context) {
+    let contextCopy = clone(context);
+    changeKey(contextCopy, 'entityEndpoint', 'endpoint');
+    let client = entitySuggest(contextCopy);
+    return (request, localContext) => { // eslint-disable-line no-unused-vars
+      return client.getLibrarySuggestions(request);
+    };
+  }
+
   return genericTransformer(librarySuggestRequest,
                             librarySuggestResponse,
                             librarySuggestFunction);
 }
 
 
-function popSuggestRequest(request, context) { // eslint-disable-line no-unused-vars
-  let clientRequest = {query: request.q, rows: request.limit, fields: request.fields};
-  return clientRequest;
-}
-
-function popSuggestResponse(response, context) { // eslint-disable-line no-unused-vars
-  return response;
-}
-
-function popSuggestFunction(context) {
-  let contextCopy = clone(context);
-  changeKey(contextCopy, 'popEndpoint', 'endpoint');
-  let client = popSuggest(contextCopy);
-  return (request, localContext) => { // eslint-disable-line no-unused-vars
-    return client.getPopSuggestions(request);
-  };
-}
-
+/**
+* Transformer for popSuggest endpoint.
+* This transformer calls the popSuggestion client and returns solr
+* document suggestions
+*/
 export function popSuggestTransformer() {
+
+  function popSuggestRequest(request, context) { // eslint-disable-line no-unused-vars
+    let clientRequest = {query: request.q, rows: request.limit, fields: request.fields};
+    return clientRequest;
+  }
+
+  function popSuggestResponse(response, context) { // eslint-disable-line no-unused-vars
+    return response;
+  }
+
+  function popSuggestFunction(context) {
+    let contextCopy = clone(context);
+    changeKey(contextCopy, 'popEndpoint', 'endpoint');
+    let client = popSuggest(contextCopy);
+    return (request, localContext) => { // eslint-disable-line no-unused-vars
+      return client.getPopSuggestions(request);
+    };
+  }
+
   return genericTransformer(popSuggestRequest,
                             popSuggestResponse,
                             popSuggestFunction);
 }
 
 
+/**
+* Transformer for suggest endpoint.
+*
+* This transformer calls the appropriate suggesttransformer based on
+* the type parameter in the request, and returns the fetched
+* suggestions.
+*
+* Available types are: creator, library, subject and title
+*/
 export function suggestTransformer() {
 
   let common = {};
@@ -158,7 +190,7 @@ export function suggestTransformer() {
                                      limit: request.limit}};
 
     if (requestEnvelope.type === 'title') {
-      requestEnvelope.request.fields = 'display.title,fedoraPid';
+      requestEnvelope.request.fields = 'display.title,fedoraPid,display.creator,display.workType';
       requestEnvelope.request.q = 'display.title:{!complexphrase inOrder=true}' +
                                   requestEnvelope.request.q;
     }
@@ -168,7 +200,14 @@ export function suggestTransformer() {
   function suggestResponse(response, context) { // eslint-disable-line no-unused-vars
     if (common.type === 'title') {
       response = response.response.docs.map((obj) => {
-        return {str: obj['display.title'][0], id: obj.fedoraPid};
+        let retObj = {str: obj['display.title'][0], id: obj.fedoraPid};
+        if (obj.hasOwnProperty('display.creator')) {
+          retObj.creator = obj['display.creator'][0];
+        }
+        if (obj.hasOwnProperty('display.workType')) {
+          retObj.type = obj['display.workType'][0];
+        }
+        return retObj;
       });
     }
     return response;
