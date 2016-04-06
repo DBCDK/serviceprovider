d73071c7b42c6673388d2bf3253b21e250219548
flashgordon <shm@dbc.dk>
2016-04-06 14:25:17 +0200

    Changed createTest to handle nested transformers

--- src/createTest.js
+++ src/createTest.js
@@ -5,13 +5,25 @@ const utils = require('./utils.js');
 
 
 function writeTestToFile(path, content) {
-  fs.writeFile(path, content, (err) => {
-    if (err) {
-      utils.die(err);
-    }
-    console.log('Test written to ' + path); // eslint-disable-line
-  });
+  fs.writeFileSync(path, content, 'utf8');
+  console.log('Test written to ' + path); // eslint-disable-line
 }
+
+function test2Str(type, transformerName, raw, transformed) {
+
+  return ['  it(\'Testing output of ' + type + 'Transformer "' + transformerName + '"\', (done) => {',
+          '',
+          '    let ' + type + 'TransformerInput = ' + JSON.stringify(raw) + ';',
+          '    let ' + type + 'TransformerOutput = ' + JSON.stringify(transformed) + ';',
+          '',
+          '    expect(' + transformerName + '(' + type + 'TransformerInput)).to.equal(' + type + 'TransformerOutput);',
+          '    done();',
+          '  });',
+          '',
+          ''].join('\n');
+
+}
+
 
 export default function createTest(clientFunction, requestTransformer, responseTransformer,
                                    request, transformedRequest, response, transformedResponse, path, description) {
@@ -24,38 +36,42 @@ export default function createTest(clientFunction, requestTransformer, responseT
   var reqt_name = utils.functionName(requestTransformer);
   var rept_name = utils.functionName(responseTransformer);
 
-  if (description === 'undefined') {
-    description = 'Test Transformers (' + reqt_name + ' and ' + rept_name + ') used with client "' + cf_name + '"';
+  if (!fs.existsSync(path)) {
+
+    if (description === 'undefined') {
+      description = 'Test Transformers (' + reqt_name + ' and ' + rept_name + ') used with client "' + cf_name + '"';
+    }
+
+    let content = ['\'use strict\';',
+                   'import {expect} from \'chai\';',
+                   '',
+                   '// PLEASE INSERT NECESSARY REQUIRES FOR THE FOLLOWING FUNCTIONS: ',
+                   '// ' + cf_name,
+                   '// ' + reqt_name,
+                   '// ' + rept_name,
+                   '',
+                   'describe(\'' + description + '\', () => {',
+                   '',
+                   ''].join('\n') +
+                   test2Str('request', reqt_name, request, transformedRequest) +
+                   test2Str('response', rept_name, response, transformedResponse) +
+                   ['});'].join('\n');
+
+    writeTestToFile(path, content);
   }
+  else {
 
-  var content = ['\'use strict\';',
-                 'import {expect} from \'chai\';',
-                 '',
-                 '// PLEASE INSERT NECESSARY REQUIRES FOR THE FOLLOWING FUNCTIONS: ',
-                 '// ' + cf_name,
-                 '// ' + reqt_name,
-                 '// ' + rept_name,
-                 '',
-                 'describe(\'' + description + '\', () => {',
-                 '',
-                 '  it(\'Testing output of requestTransformer "' + reqt_name + '"\', (done) => {',
-                 '',
-                 '    let requestTransformerInput = ' + JSON.stringify(request) + ';',
-                 '    let requestTransformeroutput = ' + JSON.stringify(transformedRequest) + ';',
-                 '',
-                 '    expect(' + reqt_name + '(requestTransformerInput)).to.equal(requestTransformeroutput);',
-                 '    done();',
-                 '  });',
-                 '',
-                 '  it(\'Testing output of responseTransformer "' + rept_name + '"\', (done) => {',
-                 '',
-                 '    let responseTransformerInput = ' + JSON.stringify(response) + ';',
-                 '    let responseTransformeroutput = ' + JSON.stringify(transformedResponse) + ';',
-                 '',
-                 '    expect(' + rept_name + '(responseTransformerInput)).to.equal(responseTransformeroutput);',
-                 '    done();',
-                 '  });',
-                 '});'].join('\n');
+    let newContent = fs.readFileSync(path, 'utf8');
+    let contentSplit = newContent.split(/(\/\/(\/|[\s\S])*\/\/.*)/);
+    contentSplit[1] += '\n// ' + cf_name + '\n// ' + reqt_name +'\n// ' + rept_name;
+    newContent = contentSplit.join('');
 
-  writeTestToFile(path, content);
+    contentSplit = newContent.split('\n');
+    contentSplit[contentSplit.length - 2] += '\n' +
+      test2Str('request', reqt_name, request, transformedRequest) +
+      test2Str('response', rept_name, response, transformedResponse);
+
+    newContent = contentSplit.join('\n');
+    writeTestToFile(path, newContent);
+  }
 }
