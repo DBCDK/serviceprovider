3724a4c9d6a80e0d51090b3d9fdb7de2dd956a0b
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-04-26 19:39:16 +0200

    progress on automatic test generation

--- src/provider/caller.js
+++ src/provider/caller.js
@@ -1,6 +1,8 @@
 'use strict';
 import request from 'request';
 import {sendRequest} from '../services/HTTPClient.js';
+import fs from 'fs';
+import utils from '../utils';
 
 export default function caller(transformerMap, context) {
   context = Object.create(context);
@@ -9,55 +11,90 @@ export default function caller(transformerMap, context) {
   return context;
 }
 
+let randomId = () => Math.random().toString(36).slice(2,8); 
+let generatedTests = [];
+let testDev = process.env.TEST_DEV;
+console.log('testDev', testDev);
+
 function call(name, params) {
-  this.createTest = true;
+  let promise;
+
+  let mockId = JSON.stringify([name, params]);
   let outerCall = false;
+  let startTime = Date.now();
   if(!this.requestId) {
-    this.requestId = Math.random().toString(36).slice(2,8);
+    this.requestId = randomId();
     outerCall = true;
+    if(this.createTest ) {
+      this.mockRecord = {};
+    }
   }
 
-  let startTime = Date.now();
-  let prom;
-  let mockId;
-  if(this.createTest && !this.mockData) {
-    this.mockData = {};
-  }
+  let isRestCall = !this.transformerMap[name] && this[name] && this[name].url;
 
   if(this.transformerMap[name]) {
-    prom = this.transformerMap[name](params, this);
-  } else if(this[name] && this[name].url) {
+    promise = this.transformerMap[name](params, this);
+
+  } else if(isRestCall) {
     let url = this[name].url;
     if(this.mockData) {
-      mockId = JSON.stringify([name, params]);
-    }
-    if(this.mockData && this.mockData[mockId]) {
-      prom = new Promise(this.mockData[mockId]);
+      if(!this.mockData[mockId]) {
+        throw {error: 'missing mockdata', mockId: mockId};
+      }
+      promise = new Promise(this.mockData[mockId]);
     } else if(typeof params === 'string') {
-      prom = new Promise((resolve, reject) => 
+      promise = new Promise((resolve, reject) => 
           request.post(url, {form: {xml: params}}, 
             (err, _, data) => err ? reject(err) : resolve(data)));
     } else if(typeof params === 'object') {
-      prom = sendRequest(url, params);
+      promise = sendRequest(url, params);
     } else {
       throw 'call error'
     }
   } else {
     throw 'call error'
   }
-  prom.then(result => {
-    if(this.createTest) {
+
+  return promise.then(result => {
+    if(testDev && params.createTest) {
       if(mockId) {
-        this.mockData[mockId] = result;
+        this.mockRecord[mockId] = result;
       }
       if(outerCall) {
-        createTest(name, params, context, result);
+        delete params.createTest;
+        saveTest({name: name, params: params, context: this.prototype, 
+          mockData: this.mockRecord, result: result});
       }
     }
     console.log(name, this.requestId, Date.now() - startTime, typeof result);
     return result;
   });
-  return prom;
 };
-function createTest(name, params, context, result) {
+
+function saveTest(test) {
+  let mockDataFile = __dirname + '/mockData.json';
+  let mockData = JSON.parse(fs.readFileSync(mockDataFile, 'utf8'));
+  Object.assign(mockData, test.mockData);
+  let source = `'use strict';
+  import Provider from '../Provider.js';
+  import {expect, assert} from 'chai';
+  import fs from 'fs';
+
+  let mockData = JSON.parse(fs.readFileSync('../mockData.json'));
+  let provider = Provider();
+
+  describe('Automated test of the ${test.name} endpoint', () => {
+    it('returns expected response for ${JSON.stringify(test.params)}', (done) => {
+      let context = ${JSON.stringify(test.context)};
+      context.mockData = mockData;
+      provider.execute("${test.name}", ${JSON.stringify(test.params)}, context)
+        .then(result => {
+          assert.deepEqual(result,
+              ${JSON.stringify(test.result)});
+          done();
+        });
+    })
+  });`;
+  fs.writeFile(mockDataFile, JSON.stringify(mockData, null, ""));
+  fs.writeFile(`${__dirname}/__tests__/autotest_${test.name}_${Date.now()}.js`);
 }
