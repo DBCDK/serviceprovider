93b7487616a4b7759fcca485ddb18d03fdfff219
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-04-26 13:15:18 +0200

    trace provider/http-promise-calls to be used for test-generation

--- src/provider/Provider.js
+++ src/provider/Provider.js
@@ -12,6 +12,7 @@ import openSearchWorkTransformer from './neoOpenSearchWorkTransformer.js';
 import facetTransformer from './facets';
 import searchTransformer from './search';
 import workTransformer from './neoWorkTransformer';
+import caller from './caller';
 
 /**
  * Initialization of the provider and the underlying services.
@@ -38,7 +39,7 @@ export default function Provider() {
 
   // we are going to reimplement a simpler mechanism to call the transformers
   function execute(name, params, context) { // eslint-disable-line no-unused-vars
-    return transformerMap[name](params, context);
+    return caller(transformerMap, context).call(name, params);
   }
 
   function availableTransforms() {
