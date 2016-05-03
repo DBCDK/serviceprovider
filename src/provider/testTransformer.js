7586d8ffc07c1e5df8dfd28f8be9847e1290f7bd
cboesgaard <cbo@dbc.dk>
2016-05-03 19:01:15 +0200

    add+integrate simple testTransformer for performance testing (could also be used for other stuff) (#226)
    * add+integrate simple testTransformer for performance testing (could also be used for other stuff)
    
    * add+integrate simple testTransformer for performance testing (could also be used for other stuff)

---
+++ src/provider/testTransformer.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// this is a transformer used for testing
+
+// motivation: measure
+// performance of SP without doing any real work
+
+import {log} from '../utils.js';
+
+export function testTransformer(params, context) { // eslint-disable-line no-unused-vars
+  log.info('testTransformer called');
+  let now = (new Date()).toString();
+  return new Promise(resolve => {
+    return resolve({statusCode: 200,
+                    data: {date: now}});
+  });
+}
+
