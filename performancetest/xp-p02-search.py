a8ae4245136c012cbc23bfdba4c8e272477a9fee
Christian Boesgaard <cbo@dbc.dk>
2016-05-06 09:37:11 +0200

    search

---
+++ performancetest/xp-p02-search.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+times = int(os.getenv("TIMES",100))
+
+auth = {
+  "token_type": "bearer",
+  "access_token": "766b5b95d196b6472ee5248ddd0b14ce8286a0ee",
+  "expires_in": 2592000
+}
+
+token = auth['access_token']
+#url = "http://localhost:8080/v0/search"
+url = "http://xp-p02.dbc.dk:3000/v0/search"
+
+
+for i in range(times):
+    print """curl -sSL -w "\\nCURL HTTPCODE=%%{http_code} SECS=%%{time_total}\\n" -H "Authorization: Bearer %s" -H "Content-Type: application/json" -X GET -d '{ "q": "hest", "fields": ["title"], "timings":true}' "%s"
+""" % (token, url)
