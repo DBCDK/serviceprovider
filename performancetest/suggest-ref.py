1917d3c383ffa027fb1b9fbc8e4f31821a0c72d5
Christian Boesgaard <cbo@dbc.dk>
2016-05-06 09:32:42 +0200

    remove curl verbose

--- performancetest/suggest-ref.py
+++ performancetest/suggest-ref.py
@@ -6,5 +6,5 @@ import os
 times = int(os.getenv("TIMES",100))
 
 for i in range(times):
-    print """curl -s -w "\\nCURL HTTPCODE=%{http_code} SECS=%{time_total}\\n" -v -H "Content-Type: Application/json" "http://xptest.dbc.dk/ms/entity-suggest/v1/subject?query=hest"
+    print """curl -sSL -w "\\nCURL HTTPCODE=%{http_code} SECS=%{time_total}\\n" -H "Content-Type: Application/json" "http://xptest.dbc.dk/ms/entity-suggest/v1/subject?query=hest"
 """    
