8f491290942350bdb930cd106d72e7a0443aa4e2
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-05-06 09:42:33 +0200

    indent

--- src/provider/caller.js
+++ src/provider/caller.js
@@ -72,89 +72,89 @@ class Context {
     this.data = contextData;
     this.transformerMap = transformerMap;
   }
-/**
- * This is a method on the context object,
- * which allows calling other transformers, and external endpoints.
- *
- * @param this the context
- * @param name the name of the endpoint
- * @param params the parameters to pass to the endpoint
- */
-call(name, params) {
-  let promise, outerCall, startTime;
+  /**
+   * This is a method on the context object,
+   * which allows calling other transformers, and external endpoints.
+   *
+   * @param this the context
+   * @param name the name of the endpoint
+   * @param params the parameters to pass to the endpoint
+   */
+  call(name, params) {
+    let promise, outerCall, startTime;
 
-  let mockId = JSON.stringify([name, params]); // key for mock data
-  this.mockData = this.mockData || Object.assign({}, this.data.mockData || {}); // used for recording/playing mock data
-  let isRestCall = !this.transformerMap[name] && this.data[name] && this.data[name].url;
-  if (!this.requestId) {
-    this.requestId = randomId(); // identifier for request, useful for grepping etc.
-    outerCall = true;
-    this.createTest = params.createTest;
-    this.timings = params.timings;
-    this.externalCallsInProgress = 0;
-    this.externalTiming = 0;
-    startTime = Date.now();
-  }
+    let mockId = JSON.stringify([name, params]); // key for mock data
+    this.mockData = this.mockData || Object.assign({}, this.data.mockData || {}); // used for recording/playing mock data
+    let isRestCall = !this.transformerMap[name] && this.data[name] && this.data[name].url;
+    if (!this.requestId) {
+      this.requestId = randomId(); // identifier for request, useful for grepping etc.
+      outerCall = true;
+      this.createTest = params.createTest;
+      this.timings = params.timings;
+      this.externalCallsInProgress = 0;
+      this.externalTiming = 0;
+      startTime = Date.now();
+    }
 
-  if (this.transformerMap[name]) { // do call the actual transformer
-    promise = this.transformerMap[name](params, this);
+    if (this.transformerMap[name]) { // do call the actual transformer
+      promise = this.transformerMap[name](params, this);
 
-  }
-  else if (isRestCall) { // make a request to an external service
-    if (this.timings) {
-      if (this.externalCallsInProgress === 0) {
-        this.externalTiming -= Date.now();
+    }
+    else if (isRestCall) { // make a request to an external service
+      if (this.timings) {
+        if (this.externalCallsInProgress === 0) {
+          this.externalTiming -= Date.now();
+        }
+        ++this.externalCallsInProgress;
       }
-      ++this.externalCallsInProgress;
-    }
-    let url = this.data[name].url;
-    if (this.mockData && this.mockData[mockId]) {
-      promise = new Promise(resolve => resolve(this.mockData[mockId]));
-    }
-    else if (typeof params === 'string') {
-      promise = new Promise((resolve, reject) =>
-          request.post(url, {form: {xml: params}},
-            (err, _, data) => err ? reject(err) : resolve(data)));
-    }
-    else if (typeof params === 'object') {
-      promise = sendRequest(url, params);
-    }
-    else { // should never happen, can only occur if call is called with wrong parameters
-      throw 'call error: wrong parameters to endpoint';
-    }
-  }
-  else { // should never happen, - the endpoint does not exist (or has no endpoint-url defined in config)
-    throw 'call error: no such endpoint';
-  }
-
-  return promise.then(result => {
-    if (this.timings && isRestCall) {
-      --this.externalCallsInProgress;
-      if (this.externalCallsInProgress === 0) {
-        this.externalTiming += Date.now();
+      let url = this.data[name].url;
+      if (this.mockData && this.mockData[mockId]) {
+        promise = new Promise(resolve => resolve(this.mockData[mockId]));
+      }
+      else if (typeof params === 'string') {
+        promise = new Promise((resolve, reject) =>
+            request.post(url, {form: {xml: params}},
+              (err, _, data) => err ? reject(err) : resolve(data)));
+      }
+      else if (typeof params === 'object') {
+        promise = sendRequest(url, params);
+      }
+      else { // should never happen, can only occur if call is called with wrong parameters
+        throw 'call error: wrong parameters to endpoint';
       }
     }
-    if (testDev && this.createTest) { // save mock-data / create text-code
-      if (isRestCall) {
-        this.mockData[mockId] = result;
-      }
-      if (outerCall) {
-        delete params.createTest;
-        saveTest({name: name, params: params,
-          context: this.data,
-          mockData: this.mockData, result: result,
-          requestId: this.requestId});
-      }
+    else { // should never happen, - the endpoint does not exist (or has no endpoint-url defined in config)
+      throw 'call error: no such endpoint';
     }
-    if (outerCall && params.timings) {
-      result.timings = {
-        total: Date.now() - startTime,
-        external: this.externalTiming
-      };
-    }
-    return result;
-  });
-}
+
+    return promise.then(result => {
+      if (this.timings && isRestCall) {
+        --this.externalCallsInProgress;
+        if (this.externalCallsInProgress === 0) {
+          this.externalTiming += Date.now();
+        }
+      }
+      if (testDev && this.createTest) { // save mock-data / create text-code
+        if (isRestCall) {
+          this.mockData[mockId] = result;
+        }
+        if (outerCall) {
+          delete params.createTest;
+          saveTest({name: name, params: params,
+            context: this.data,
+            mockData: this.mockData, result: result,
+            requestId: this.requestId});
+        }
+      }
+      if (outerCall && params.timings) {
+        result.timings = {
+          total: Date.now() - startTime,
+          external: this.externalTiming
+        };
+      }
+      return result;
+    });
+  }
 }
 
 /**
