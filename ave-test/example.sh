ea94fa62f1fe3e7f3cd8f04d6d59bb06518e7abe
Christian Boesgaard <cbo@dbc.dk>
2016-05-09 17:07:06 +0200

    example for ave (dir should have better name)

---
+++ ave-test/example.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+export URL='http://platformbe-p01.dbc.dk:8080/v0/'
+export TOKEN='766b5b95d196b6472ee5248ddd0b14ce8286a0ee'
+
+export ENDPOINT='search'
+#export REQUEST='{ "q": "hest", "fields": ["title"], "createTest":true, "pretty":true}'
+
+export REQUEST='
+{ 
+"q": "hest", 
+"fields": ["title"], 
+"createTest":true, "pretty":true
+}'
+
+
+curl -sSL -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -X GET -d "$REQUEST" $URL$ENDPOINT
+
