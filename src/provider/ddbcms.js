5cb222ea6fed5afde7d01966af11439e96f7d0fa
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-05-09 11:35:12 +0200

    implementation of /news and /events endpoints

--- src/provider/ddbcms.js
+++ src/provider/ddbcms.js
@@ -6,3 +6,27 @@ export function ddbcms(params, context) {
   params.key = config.password;
   return context.query(config.url + 'content/fetch', params);
 }
+
+let ddbResponse = (result) => ({
+  statusCode: 200,
+  data: result.data.items.map(o => Object.assign(o.fields, {nid: o.nid}))
+});
+
+let ddbRequest = (type) => (params, context) => {
+  if (Array.isArray(params.nids)) {
+    return Promise.all(params.nids.map(nid => ddbcms({node: nid}, context)
+          .then(ddbResponse)))
+      .then(result => ({
+        statusCode: 200,
+        data: result.map(o => o.data[0] || null)
+      }));
+  }
+
+  return ddbcms({
+    type: type,
+    amount: (typeof params.limit === 'undefined') ? 10 : params.limit,
+    skip: (typeof params.offset === 'undefined') ? 0 : params.offset
+  }, context).then(ddbResponse);
+};
+export const news = ddbRequest('ding_news');
+export const events = ddbRequest('ding_event');
