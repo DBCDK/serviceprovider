e6f0a510612ec17d267eee9be07680e394ebf7dc
Christian Boesgaard <cbo@dbc.dk>
2016-05-17 11:01:14 +0200

    test fixed

--- performancetest/all-tests.sh
+++ performancetest/all-tests.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 export TIMES=1000
-export URL='http://xp-p02.dbc.dk:3000/v0/'
+#export URL='http://xp-p02.dbc.dk:3000/v0/'
+export URL='http://localhost:3000/v0/'
 export ACCESS_TOKEN='64141bea42b72443ce3848455f5960e1438fb434'
-export JOBS=8
+#export URL='http://localhost:8080/v0/'
+#export ACCESS_TOKEN='766b5b95d196b6472ee5248ddd0b14ce8286a0ee'
+export JOBS=1
+export PRE='local-'
 
 
 export ENDPOINT='search'
@@ -37,7 +41,7 @@ export REQUEST='{"q" : "harry", "timings":true}'
 ./make-test.sh
 
 export ENDPOINT='libraries'
-export REQUEST='{"limit": 10, "agencyIds": ["717500"], "fields": ["agencyId", "orderParameters"], "timings":true}'
+export REQUEST='{"agencyIds": ["717500"], "fields": ["agencyId", "orderParameters"], "timings":true}'
 ./make-test.sh
 
 
