6906a3ae3c5a65319d11e314e0ac7ca7174a1a4c
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-05-17 18:25:52 +0200

    code for validating the context (forgot to add it in previous commit)

---
+++ src/provider/validateContext.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let contextSample = require('../../context-sample.json').context;
+
+export default (context) => {
+  for (let key1 in contextSample) {
+    if (typeof context[key1] !== 'object') {
+      throw `Auth-config problem: ${key1} is not an object in the context`;
+    }
+    for (let key2 in contextSample[key1]) {
+      if (typeof context[key1][key2] === 'undefined') {
+        throw `Auth-config problem: missing ${key2} in context.${key1}`;
+      }
+    }
+  }
+};
