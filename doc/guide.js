112aaa20780577a5fcc35719f033f5b960b578aa
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-05-20 11:11:54 +0200

    progress on guide

--- doc/guide.js
+++ doc/guide.js
@@ -1,10 +1,12 @@
 // # Quick start guide to the open platform
 //
+// TODO: `%s/http...localhost.8080/https:\/\/openplatform.dbc.dk/g`
+//
 // This introduction to getting started with the open platform is online on:
-// [https://openplatform.dbc.dk/v0/guide.html#id:secret](https://openplatform.dbc.dk/v0/guide.html#id:secret).
+// [http://localhost:8080/v0/guide.html#id:secret](http://localhost:8080/v0/guide.html#id:secret).
 //
 // It is written as a literate JavaScript
-// [source file](https://openplatform.dbc.dk/v0/guide.js),
+// [source file](http://localhost:8080/v0/guide.js),
 // that can be executed directly in the browser.
 // Use your client id/secret in the url, and open the browser console
 // to see the result of the examples.
@@ -29,11 +31,15 @@
 // In browser JavaScript this is done with XHR.
 
 var xhr = new XMLHttpRequest();
+
 xhr.open('POST', 'https://auth.dbc.dk/oauth/token');
-xhr.setRequestHeader('Authorization',
-    'Basic ' + btoa(client_id() + ':' + client_secret()));
+
+xhr.setRequestHeader('Authorization', 'Basic ' + 
+    btoa(client_id() + ':' + client_secret()));
+
 xhr.setRequestHeader('Content-Type',
     'application/x-www-form-urlencoded');
+
 xhr.send('grant_type=password' +
     '&username=' + user_id() +
     '&password=' + user_password());
@@ -45,7 +51,7 @@ xhr.send('grant_type=password' +
 // The rest of this guide assume that we have a valid `access_token`
 
 xhr_promise(xhr).then(function(result) {
-  var access_token = result.access_token;
+    var access_token = result.access_token;
 
 // ## Renewal of token
 //
@@ -57,19 +63,40 @@ xhr_promise(xhr).then(function(result) {
 
 // # Examples of api-usage
 //
-// Requests to the API can sent by POSTing a JSON object
-// to the API endpoint.
-// TODO
+// Requests to the API can be sent by POSTing a JSON object
+// to the API endpoint:
 
-function openplatform(endpoint, parameters) {
-  xhr = new XMLHttpRequest();
-  parameters.access_token = access_token;
-  xhr.open('POST', 'https://openplatform.dbc.dk/v0/' + endpoint);
-  xhr.send(JSON.stringify(parameters));
-  return xhr_promise(xhr);
+HTTP_POST('http://localhost:8080/v0/suggest',
+    { q: 'Steppe', type: 'title', 
+      access_token: access_token});
+
+HTTP_POST('http://localhost:8080/v0/search',
+    {q: 'ost', access_token: access_token});
+
+// And all of the other APIs also have the same structure
+// of sending a JSON object of parameters to the endpoint
+// and then getting the result back.
+//
+// You can also express the entire query in the url,
+// which is useful for interactive testing. Ie.
+// <a id=sample_url></a>
+//
+//
+// 
+//
+// In browser JavaScript, HTTP-POSTing a JSON object
+// is done like this:
+
+function HTTP_POST(url, parameters) {
+    xhr = new XMLHttpRequest();
+    parameters.access_token = access_token;
+    xhr.open('POST', url);
+    xhr.setRequestHeader('Content-Type',
+        'application/json');
+    xhr.send(JSON.stringify(parameters));
+    return xhr_promise(xhr);
 }
 
-openplatform('search', {q: 'harry'});
 
 // ... search, recommendations, ...
 //
