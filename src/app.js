865a85f72dcf2d78c500fc56c50805e7f93892f7
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-05-23 20:31:16 +0200

    cors, fixes #399

--- src/app.js
+++ src/app.js
@@ -52,7 +52,8 @@ module.exports.run = function (worker) {
   // Enable CORS
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
 
