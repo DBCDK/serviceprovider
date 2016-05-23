f0775ad50d2fe0cc94b532c7a7ea951415da0e74
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-05-23 14:39:12 +0200

    start js-api

---
+++ client/build.sh
@@ -0,0 +1,3 @@
+../node_modules/.bin/browserify dbcopenplatform.js | 
+../node_modules/.bin/uglifyjs  |
+cat > dbcopenplatform.min.js
---
+++ client/dbcopenplatform.js
@@ -0,0 +1,8 @@
+(function() {
+  var cycle = require('cycle');
+  var sc = require('socketcluster-client');
+  function openplatform() {
+  }
+
+  window.DbcOpenPlatform = openplatform;
+})();
