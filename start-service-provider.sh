da48bea92c85a8313da97b274017c8a8c1d39a76
cboesgaard <cbo@dbc.dk>
2016-05-25 13:35:03 +0200

    move start commands to start SP to seperate script (#439)
    * performance generators for prod
    
    * move start commands to start SP to seperate script
    
    * + fix travis

---
+++ start-service-provider.sh
@@ -0,0 +1,3 @@
+PORT=3000 node src/smaug/minismaug.js -f context-sample.json & sleep 2
+SMAUG=http://localhost:3000 MOCK_FILE=apitest/mockdata.json SWAGGER_HTTP=true TEST_DEV=true node src/main.js & sleep 10
+
