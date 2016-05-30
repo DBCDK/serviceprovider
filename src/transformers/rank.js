ad8d63a46f9ff8c5341fdfb1f7eb9fad15c2f9e3
Christian Boesgaard <cbo@dbc.dk>
2016-05-30 15:01:05 +0200

    rank changed to use new context + test updated

--- src/transformers/rank.js
+++ src/transformers/rank.js
@@ -2,7 +2,7 @@
 
 function createRequestParameters(request, context) {
   // create request parameters from request and context
-  const uri = context.get('rank.url');
+  const uri = context.get('services.rank');
   let paramsPost = {
     uri: uri,
     method: 'POST',
