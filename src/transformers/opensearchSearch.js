cef43b84eb08c5a767e92356e630c3c2f1a62fa3
Jesper Damkjaer <jda@dbc.dk>
2016-05-30 21:13:14 +0200

    changed context.data... to context.get for search.

--- src/transformers/opensearchSearch.js
+++ src/transformers/opensearchSearch.js
@@ -7,8 +7,8 @@ export default (params, context) => {
     return Promise.resolve({statusCode: 400, error: 'missing q parameter'});
   }
 
-  let agency = context.data.opensearch.agency;
-  let profile = context.data.opensearch.profile;
+  let agency = context.get('search.agency');
+  let profile = context.get('search.profile');
   let q = params.q.replace(/</g, '&lt;');
   let sort = (params.sort || '').replace(/</g, '&lt;');
   let offset = 1 + (parseInt(params.offset, 10) || 0);
