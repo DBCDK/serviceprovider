b2dfe519ae2555fa58267d1119f04e632784f120
RasmusErik Voel Jensen <github-rasmuserik@solsort.dk>
2016-05-31 13:08:05 +0200

    bump version number to v1 (#523)

--- ave-test/example.sh
+++ ave-test/example.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export URL='http://platformbe-p01.dbc.dk:8080/v0/'
+export URL='http://platformbe-p01.dbc.dk:8080/v1/'
 export TOKEN='766b5b95d196b6472ee5248ddd0b14ce8286a0ee'
 
 export ENDPOINT='search'
--- client/dbc_openplatform.js
+++ client/dbc_openplatform.js
@@ -113,7 +113,7 @@
             hostname: 'openplatform.dbc.dk', 
             port: 443,
             secure: true,
-            path: '/v0/socketcluster/?access_token=' + token
+            path: '/v1/socketcluster/?access_token=' + token
           });
         sc.on('connectAbort', function(result) {
           reject(result);
