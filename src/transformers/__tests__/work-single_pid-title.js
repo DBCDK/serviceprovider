fd8c76cbcfa16699e770493221a02ec57c2565ee
Jesper Damkjaer <damkjaer.jesper@gmail.com>
2016-05-31 13:28:43 +0200

    Added tests for work. This fixes #437 (#524)

---
+++ src/transformers/__tests__/work-single_pid-title.js
@@ -0,0 +1,25 @@
+/* eslint-disable max-len, quotes, comma-spacing, key-spacing, quote-props, indent */
+// Request: work {"pids":["870970-basis:28448716"],"fields":["title"]}
+'use strict';
+import Provider from '../../provider/Provider.js';
+import {assert, fail} from 'chai';
+
+let context = {"services":{"ddbcmsapi":"http://rest.filmstriben.dbc.inlead.dk/web/","moreinfo":"http://moreinfo.addi.dk/2.6/","openagency":"http://openagency.addi.dk/2.24/","openholdingstatus":"https://openholdingstatus.addi.dk/2.2/","openorder":"https://openorder.addi.dk/2.7.1/","TESTopenorder":"https://openorder.addi.dk/test_2.7.1/","opensearch":"http://opensearch.addi.dk/b3.0_4.2/","openuserstatus":"https://openuserstatus.addi.dk/1.4.1/","rank":"https://xptest.dbc.dk/ms/rank/v1","suggestpopular":"http://xptest.dbc.dk/ms/entity-pop/v1","suggestcreator":"http://xptest.dbc.dk/ms/entity-suggest/v1/creator","suggestlibrary":"http://xptest.dbc.dk/ms/entity-suggest/v1/library","suggestsubject":"http://xptest.dbc.dk/ms/entity-suggest/v1/subject","recommendurls":{"default":"https://xptest.dbc.dk/ms/recommend-cosim/v1","popular":"https://xptest.dbc.dk/ms/recommend-pop/v1"}},"search":{"agency":"775100","profile":"opac","collectionidentifiers":"rec.collectionIdentifier:150013-palle OR rec.collectionIdentifier:758000-katalog"},"netpunkt":{"user":"XXXXX","group":"XXXXX","password":"XXXXX"},"user":{"agency":"775100","librarytype":"folkebibliotek","id":"XXXXX","pin":"XXXXX","salt":"XXXXX"},"app":{"clientid":"XXXXX","ddbcmsapipassword":"XXXXX","orderpolicyrequester":"190101"}};
+let provider = Provider();
+let mockData = {"[\"opensearch\",{\"qs\":{\"action\":\"getObject\",\"identifier\":[\"870970-basis:28448716\"],\"agency\":\"775100\",\"profile\":\"opac\",\"outputType\":\"json\",\"objectFormat\":[\"briefDisplay\"]}}]":"{\"searchResponse\":{\"result\":{\"hitCount\":{\"$\":\"1\"},\"collectionCount\":{\"$\":\"1\"},\"more\":{\"$\":\"false\"},\"searchResult\":[{\"collection\":{\"resultPosition\":{\"$\":\"1\"},\"numberOfObjects\":{\"$\":\"1\"},\"object\":[{\"identifier\":{\"$\":\"870970-basis:28448716\"},\"primaryObjectIdentifier\":{\"$\":\"870970-basis:28448716\"},\"recordStatus\":{\"$\":\"active\"},\"creationDate\":{\"$\":\"2010-09-28\"},\"formatsAvailable\":{\"format\":[{\"$\":\"dkabm\"},{\"$\":\"marcxchange\"}]},\"objectsAvailable\":{\"identifier\":[{\"$\":\"775100-katalog:28448716\"},{\"$\":\"870970-basis:28448716\"}]}}]},\"formattedCollection\":{\"briefDisplay\":{\"manifestation\":[{\"accessType\":{\"$\":\"physical\"},\"creator\":{\"$\":\"Dennis J\\u00fcrgensen\"},\"fedoraPid\":{\"$\":\"870970-basis:28448716\"},\"identifier\":{\"$\":\"870970-basis:28448716\"},\"language\":{\"$\":\"Dansk\"},\"title\":{\"$\":\"Kadavermarch\"},\"titleFull\":{\"$\":\"Kadavermarch\"},\"type\":{\"$\":\"Bog\"},\"workType\":{\"$\":\"book\"}}]}}}],\"facetResult\":{\"$\":\"\"},\"statInfo\":{\"fedoraRecordsCached\":{\"$\":\"1\"},\"fedoraRecordsRead\":{\"$\":\"5\"},\"time\":{\"$\":\"0.110063\"},\"trackingId\":{\"$\":\"2016-05-31T13:01:47:325598:1578\"}}}},\"@namespaces\":{\"ac\":\"http:\\/\\/biblstandard.dk\\/ac\\/namespace\\/\",\"dbcaddi\":\"http:\\/\\/oss.dbc.dk\\/rdf\\/dbcaddi#\",\"dbcbib\":\"http:\\/\\/oss.dbc.dk\\/rdf\\/dbcbib#\",\"dc\":\"http:\\/\\/purl.org\\/dc\\/elements\\/1.1\\/\",\"dcmitype\":\"http:\\/\\/purl.org\\/dc\\/dcmitype\\/\",\"dcterms\":\"http:\\/\\/purl.org\\/dc\\/terms\\/\",\"dkabm\":\"http:\\/\\/biblstandard.dk\\/abm\\/namespace\\/dkabm\\/\",\"dkdcplus\":\"http:\\/\\/biblstandard.dk\\/abm\\/namespace\\/dkdcplus\\/\",\"docbook\":\"http:\\/\\/docbook.org\\/ns\\/docbook\",\"kml\":\"http:\\/\\/www.opengis.net\\/kml\\/2.2\",\"marcx\":\"info:lc\\/xmlns\\/marcxchange-v1\",\"mx\":\"http:\\/\\/www.loc.gov\\/MARC21\\/slim\",\"of\":\"http:\\/\\/oss.dbc.dk\\/ns\\/openformat\",\"ofo\":\"http:\\/\\/oss.dbc.dk\\/ns\\/openformatoutput\",\"os\":\"http:\\/\\/oss.dbc.dk\\/ns\\/opensearch\",\"oso\":\"http:\\/\\/oss.dbc.dk\\/ns\\/opensearchobjects\",\"oss\":\"http:\\/\\/oss.dbc.dk\\/ns\\/osstypes\",\"xs\":\"http:\\/\\/www.w3.org\\/2001\\/XMLSchema\",\"xsi\":\"http:\\/\\/www.w3.org\\/2001\\/XMLSchema-instance\"}}"};
+
+describe('Automated test: work-single_pid-title', () => {
+  it('expected response. ID:cuv982, for {"pids":["870970-basis:28448716"],"fields":["title"]}', (done) => {
+    context.mockData = mockData;
+    provider.execute('work', {"pids":["870970-basis:28448716"],"fields":["title"]}, context)
+      .then(result => {
+        assert.deepEqual(result,
+            {"statusCode":200,"data":[{"accessType":["physical"],"creator":["Dennis Jürgensen"],"fedoraPid":["870970-basis:28448716"],"pid":["870970-basis:28448716"],"language":["Dansk"],"title":["Kadavermarch"],"titleFull":["Kadavermarch"],"type":["Bog"],"workType":["book"]}]});
+        done();
+      })
+      .catch(result => {
+        fail({throw: result}, {"statusCode":200,"data":[{"accessType":["physical"],"creator":["Dennis Jürgensen"],"fedoraPid":["870970-basis:28448716"],"pid":["870970-basis:28448716"],"language":["Dansk"],"title":["Kadavermarch"],"titleFull":["Kadavermarch"],"type":["Bog"],"workType":["book"]}]});
+        done();
+      });
+  });
+});
