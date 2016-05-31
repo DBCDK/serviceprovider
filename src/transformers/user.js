DBCDK/serviceprovider

commit d05bfba9e4a837247b99821d73d2a3b14a42839e

updated user endpoint to handle debt (#515)
* fixed code to use new context, and removed direct context access

* fixed code to use new context, and removed direct context access

* updated user endpoint to handle debt

* updated user endpoint to handle debt

src/transformers/user.js
@@ -25,6 +25,24 @@ function loan(loanItem) {
 
 
 /**
+ * Maps debt item from backend response to serviceprovider api
+ * @param {Object} obj openuserstatus orders response
+ * @returns response with mapped keys
+ */
+function debt(debtItem) {
+
+  let result = {amount: debtItem.fiscalTransactionAmount.$,
+                currency: debtItem.fiscalTransactionCurrency.$,
+                date: debtItem.fiscalTransactionDate.$.split('T')[0],
+                title: debtItem.title.$};
+  if (debtItem.author) {
+    result.author = debtItem.author.$;
+  }
+  return result;
+}
+
+
+/**
  * Maps order item from backend response to serviceprovider api
  * @param {Object} obj openuserstatus orders response
  * @returns response with mapped keys
@@ -84,11 +102,18 @@ export default (request, context) => {
     if (body.data.getUserStatusResponse.userStatus.orderedItems.order) {
       orders = body.data.getUserStatusResponse.userStatus.orderedItems.order;
     }
+    
+    let debts = [];
+    if (body.data.getUserStatusResponse.userStatus.fiscalAccount) {
+      debts = body.data.getUserStatusResponse.userStatus.fiscalAccount.fiscalTransaction;
+    }
 
     let data = {id: id.toString('base64'),
                 loans: loans.map(loan),
-                orders: orders.map(order)
+                orders: orders.map(order),
+                debt: debts.map(debt)
                };
+
     if (context.get('services.ddbcmsapi')) {
       data.ddbcmsapi = context.get('services.ddbcmsapi');
     }

doc/spec.yaml
@@ -365,12 +365,11 @@ api:
             creator: Jonasson, Jonas
             status: In process
         debt:
-          amount: "20"
-          currency: dkk
-          date: 2015-11-21
-          type: Fine
-          author: Kristín Marja Baldursdóttir
-          title: Kantate
+          - amount: "20"
+            currency: DKK
+            date: 2015-11-21
+            author: Kristín Marja Baldursdóttir
+            title: Kantate
   work:
     tags:
       - Search

src/transformers/__tests__/user-get-user-info.js
@@ -6,19 +6,19 @@ import {assert, fail} from 'chai';
 
 let context = {"services":{"ddbcmsapi":"http://rest.filmstriben.dbc.inlead.dk/web/","moreinfo":"http://moreinfo.addi.dk/2.6/","openagency":"http://openagency.addi.dk/2.24/","openholdingstatus":"https://openholdingstatus.addi.dk/2.2/","openorder":"https://openorder.addi.dk/2.7.1/","TESTopenorder":"https://openorder.addi.dk/test_2.7.1/","opensearch":"http://opensearch.addi.dk/b3.0_4.2/","openuserstatus":"https://openuserstatus.addi.dk/1.4.1/","rank":"https://xptest.dbc.dk/ms/rank/v1","suggestpopular":"http://xptest.dbc.dk/ms/entity-pop/v1","suggestcreator":"http://xptest.dbc.dk/ms/entity-suggest/v1/creator","suggestlibrary":"http://xptest.dbc.dk/ms/entity-suggest/v1/library","suggestsubject":"http://xptest.dbc.dk/ms/entity-suggest/v1/subject","recommendurls":{"default":"https://xptest.dbc.dk/ms/recommend-cosim/v1","popular":"https://xptest.dbc.dk/ms/recommend-pop/v1"}},"search":{"agency":"775100","profile":"opac","collectionidentifiers":"rec.collectionIdentifier:150013-palle OR rec.collectionIdentifier:758000-katalog"},"netpunkt":{"user":"XXXXX","group":"XXXXX","password":"XXXXX"},"user":{"agency":"100451","id":"XXXXX","pin":"XXXXX","salt":"XXXXX"},"app":{"clientid":"XXXXX","ddbcmsapipassword":"XXXXX","orderpolicyrequester":"190101"}};
 let provider = Provider();
-let mockData = {"[\"openuserstatus\",{\"qs\":{\"agencyId\":\"100451\",\"userId\":\"XXXXX\",\"userPincode\":\"XXXXX\",\"authentication.groupIdAut\":\"XXXXX\",\"authentication.passwordAut\":\"XXXXX\",\"authentication.userIdAut\":\"XXXXX\",\"action\":\"getUserStatus\",\"outputType\":\"json\"}}]":"{\"getUserStatusResponse\":{\"userId\":{\"$\":\"XXXXX\",\"@\":\"ous\"},\"userStatus\":{\"loanedItems\":{\"loan\":[{\"title\":{\"$\":\"9900.\",\"@\":\"ous\"},\"dateDue\":{\"$\":\"2016-06-20T00:00:00+02:00\",\"@\":\"ous\"},\"loanId\":{\"$\":\"NCIPMDAxOXw5OTAwfHw5OTAwLnx8\",\"@\":\"ous\"},\"@\":\"ous\"},{\"author\":{\"$\":\"Rosen, Michael\",\"@\":\"ous\"},\"title\":{\"$\":\"Smut\",\"@\":\"ous\"},\"dateDue\":{\"$\":\"2016-06-27T00:00:00+02:00\",\"@\":\"ous\"},\"loanId\":{\"$\":\"NCIPMDAxOXwwMDAwMDMyNDg0fFJvc2VuLCBNaWNoYWVsfFNtdXR8fHw=\",\"@\":\"ous\"},\"@\":\"ous\"}],\"loansCount\":{\"$\":\"2\",\"@\":\"ous\"},\"@\":\"ous\"},\"orderedItems\":{\"order\":[{\"author\":{\"$\":\"Loumann, Ole\",\"@\":\"ous\"},\"title\":{\"$\":\"Turen g\\u00e5r til Gran Canaria, Fuerteventura og Lanzarote\",\"@\":\"ous\"},\"orderDate\":{\"$\":\"2016-05-27T00:00:00+02:00\",\"@\":\"ous\"},\"orderId\":{\"$\":\"NCIPMDAxOXwyNy0wNS0yMDE2IDEwOjA3OjU4fExvdW1hbm4sIE9sZXxUdXJlbiBnJmFyaW5nO3IgdGlsIEdyYW4gQ2FuYXJpYSwgRnVlcnRldmVudHVyYSBvZyBMYW56YXJvdGV8fHwx\",\"@\":\"ous\"},\"orderStatus\":{\"$\":\"In process\",\"@\":\"ous\"},\"orderType\":{\"$\":\"Hold\",\"@\":\"ous\"},\"holdQueuePosition\":{\"$\":\"1\",\"@\":\"ous\"},\"pickUpAgency\":{\"$\":\"DK-100451\",\"@\":\"ous\"},\"@\":\"ous\"},{\"author\":{\"$\":\"Gerlow, Jeanette \\u00d8bro\",\"@\":\"ous\"},\"title\":{\"$\":\"Det \\u00b7norske job\",\"@\":\"ous\"},\"orderDate\":{\"$\":\"2016-05-27T00:00:00+02:00\",\"@\":\"ous\"},\"orderId\":{\"$\":\"NCIPMDAxOXwyNy0wNS0yMDE2IDEwOjA0OjAyfEdlcmxvdywgSmVhbmV0dGUgJk9zbGFzaDticm98RGV0ICZtaWRkb3Q7bm9yc2tlIGpvYnx8fA==\",\"@\":\"ous\"},\"orderStatus\":{\"$\":\"In process\",\"@\":\"ous\"},\"orderType\":{\"$\":\"Hold\",\"@\":\"ous\"},\"pickUpDate\":{\"$\":\"2016-05-27T00:00:00+02:00\",\"@\":\"ous\"},\"pickUpExpiryDate\":{\"$\":\"2016-06-06T00:00:00+02:00\",\"@\":\"ous\"},\"pickUpAgency\":{\"$\":\"DK-100451\",\"@\":\"ous\"},\"@\":\"ous\"}],\"ordersCount\":{\"$\":\"2\",\"@\":\"ous\"},\"@\":\"ous\"},\"@\":\"ous\"},\"@\":\"ous\"},\"@namespaces\":{\"ous\":\"http:\\/\\/oss.dbc.dk\\/ns\\/openuserstatus\"}}"};
+let mockData = {"[\"openuserstatus\",{\"qs\":{\"agencyId\":\"100451\",\"userId\":\"XXXXX\",\"userPincode\":\"XXXXX\",\"authentication.groupIdAut\":\"XXXXX\",\"authentication.passwordAut\":\"XXXXX\",\"authentication.userIdAut\":\"XXXXX\",\"action\":\"getUserStatus\",\"outputType\":\"json\"}}]":"{\"getUserStatusResponse\":{\"userId\":{\"$\":\"XXXXX\",\"@\":\"ous\"},\"userStatus\":{\"loanedItems\":{\"loan\":[{\"title\":{\"$\":\"9900.\",\"@\":\"ous\"},\"dateDue\":{\"$\":\"2016-06-20T00:00:00+02:00\",\"@\":\"ous\"},\"loanId\":{\"$\":\"NCIPMDAxOXw5OTAwfHw5OTAwLnx8\",\"@\":\"ous\"},\"@\":\"ous\"},{\"author\":{\"$\":\"Rosen, Michael\",\"@\":\"ous\"},\"title\":{\"$\":\"Smut\",\"@\":\"ous\"},\"dateDue\":{\"$\":\"2016-06-29T00:00:00+02:00\",\"@\":\"ous\"},\"loanId\":{\"$\":\"NCIPMDAxOXwwMDAwMDMyNDg0fFJvc2VuLCBNaWNoYWVsfFNtdXR8fHw=\",\"@\":\"ous\"},\"@\":\"ous\"}],\"loansCount\":{\"$\":\"2\",\"@\":\"ous\"},\"@\":\"ous\"},\"orderedItems\":{\"order\":[{\"author\":{\"$\":\"Loumann, Ole\",\"@\":\"ous\"},\"title\":{\"$\":\"Turen g\\u00e5r til Gran Canaria, Fuerteventura og Lanzarote\",\"@\":\"ous\"},\"orderDate\":{\"$\":\"2016-05-27T00:00:00+02:00\",\"@\":\"ous\"},\"orderId\":{\"$\":\"NCIPMDAxOXwyNy0wNS0yMDE2IDEwOjA3OjU4fExvdW1hbm4sIE9sZXxUdXJlbiBnJmFyaW5nO3IgdGlsIEdyYW4gQ2FuYXJpYSwgRnVlcnRldmVudHVyYSBvZyBMYW56YXJvdGV8fHwx\",\"@\":\"ous\"},\"orderStatus\":{\"$\":\"In process\",\"@\":\"ous\"},\"orderType\":{\"$\":\"Hold\",\"@\":\"ous\"},\"holdQueuePosition\":{\"$\":\"1\",\"@\":\"ous\"},\"pickUpAgency\":{\"$\":\"DK-100451\",\"@\":\"ous\"},\"@\":\"ous\"}],\"ordersCount\":{\"$\":\"1\",\"@\":\"ous\"},\"@\":\"ous\"},\"@\":\"ous\"},\"@\":\"ous\"},\"@namespaces\":{\"ous\":\"http:\\/\\/oss.dbc.dk\\/ns\\/openuserstatus\"}}"};
 
 describe('Automated test: user-get-user-info', () => {
-  it('expected response. ID:1srre6, for {}', (done) => {
+  it('expected response. ID:7j9nzx, for {}', (done) => {
     context.mockData = mockData;
     provider.execute('user', {}, context)
       .then(result => {
         assert.deepEqual(result,
-            {"statusCode":200,"data":{"id":"KbZ0UoBGys8QMLzieTQ5mHZ39ivzulP4","loans":[{"loanId":"NCIPMDAxOXw5OTAwfHw5OTAwLnx8","dueDate":"2016-06-20T00:00:00+02:00","title":"9900."},{"loanId":"NCIPMDAxOXwwMDAwMDMyNDg0fFJvc2VuLCBNaWNoYWVsfFNtdXR8fHw=","dueDate":"2016-06-27T00:00:00+02:00","title":"Smut","author":"Rosen, Michael"}],"orders":[{"title":"Turen går til Gran Canaria, Fuerteventura og Lanzarote","orderId":"Hold:NCIPMDAxOXwyNy0wNS0yMDE2IDEwOjA3OjU4fExvdW1hbm4sIE9sZXxUdXJlbiBnJmFyaW5nO3IgdGlsIEdyYW4gQ2FuYXJpYSwgRnVlcnRldmVudHVyYSBvZyBMYW56YXJvdGV8fHwx","orderDate":"2016-05-27T00:00:00+02:00","status":"In process","library":"DK-100451","holdQueuePosition":"1","author":"Loumann, Ole"},{"title":"Det ·norske job","orderId":"Hold:NCIPMDAxOXwyNy0wNS0yMDE2IDEwOjA0OjAyfEdlcmxvdywgSmVhbmV0dGUgJk9zbGFzaDticm98RGV0ICZtaWRkb3Q7bm9yc2tlIGpvYnx8fA==","orderDate":"2016-05-27T00:00:00+02:00","status":"In process","library":"DK-100451","author":"Gerlow, Jeanette Øbro"}],"ddbcmsapi":"http://rest.filmstriben.dbc.inlead.dk/web/"}});
+            {"statusCode":200,"data":{"id":"KbZ0UoBGys8QMLzieTQ5mHZ39ivzulP4","loans":[{"loanId":"NCIPMDAxOXw5OTAwfHw5OTAwLnx8","dueDate":"2016-06-20T00:00:00+02:00","title":"9900."},{"loanId":"NCIPMDAxOXwwMDAwMDMyNDg0fFJvc2VuLCBNaWNoYWVsfFNtdXR8fHw=","dueDate":"2016-06-29T00:00:00+02:00","title":"Smut","author":"Rosen, Michael"}],"orders":[{"title":"Turen går til Gran Canaria, Fuerteventura og Lanzarote","orderId":"Hold:NCIPMDAxOXwyNy0wNS0yMDE2IDEwOjA3OjU4fExvdW1hbm4sIE9sZXxUdXJlbiBnJmFyaW5nO3IgdGlsIEdyYW4gQ2FuYXJpYSwgRnVlcnRldmVudHVyYSBvZyBMYW56YXJvdGV8fHwx","orderDate":"2016-05-27T00:00:00+02:00","status":"In process","library":"DK-100451","holdQueuePosition":"1","author":"Loumann, Ole"}],"debt":[],"ddbcmsapi":"http://rest.filmstriben.dbc.inlead.dk/web/"}});
         done();
       })
       .catch(result => {
-        fail({throw: result}, {"statusCode":200,"data":{"id":"zeEOALqmSuy+ysfz0zYbna0CgwtrhewG","loans":[{"loanId":"NCIPMDAxOXw5OTAwfHw5OTAwLnx8","dueDate":"2016-06-20T00:00:00+02:00","title":"9900."},{"loanId":"NCIPMDAxOXwwMDAwMDMyNDg0fFJvc2VuLCBNaWNoYWVsfFNtdXR8fHw=","dueDate":"2016-06-27T00:00:00+02:00","title":"Smut","author":"Rosen, Michael"}],"orders":[{"title":"Turen går til Gran Canaria, Fuerteventura og Lanzarote","orderId":"Hold:NCIPMDAxOXwyNy0wNS0yMDE2IDEwOjA3OjU4fExvdW1hbm4sIE9sZXxUdXJlbiBnJmFyaW5nO3IgdGlsIEdyYW4gQ2FuYXJpYSwgRnVlcnRldmVudHVyYSBvZyBMYW56YXJvdGV8fHwx","orderDate":"2016-05-27T00:00:00+02:00","status":"In process","library":"DK-100451","holdQueuePosition":"1","author":"Loumann, Ole"},{"title":"Det ·norske job","orderId":"Hold:NCIPMDAxOXwyNy0wNS0yMDE2IDEwOjA0OjAyfEdlcmxvdywgSmVhbmV0dGUgJk9zbGFzaDticm98RGV0ICZtaWRkb3Q7bm9yc2tlIGpvYnx8fA==","orderDate":"2016-05-27T00:00:00+02:00","status":"In process","library":"DK-100451","author":"Gerlow, Jeanette Øbro"}],"ddbcmsapi":"http://rest.filmstriben.dbc.inlead.dk/web/"}});
+        fail({throw: result}, {"statusCode":200,"data":{"id":"KbZ0UoBGys8QMLzieTQ5mHZ39ivzulP4","loans":[{"loanId":"NCIPMDAxOXw5OTAwfHw5OTAwLnx8","dueDate":"2016-06-20T00:00:00+02:00","title":"9900."},{"loanId":"NCIPMDAxOXwwMDAwMDMyNDg0fFJvc2VuLCBNaWNoYWVsfFNtdXR8fHw=","dueDate":"2016-06-29T00:00:00+02:00","title":"Smut","author":"Rosen, Michael"}],"orders":[{"title":"Turen går til Gran Canaria, Fuerteventura og Lanzarote","orderId":"Hold:NCIPMDAxOXwyNy0wNS0yMDE2IDEwOjA3OjU4fExvdW1hbm4sIE9sZXxUdXJlbiBnJmFyaW5nO3IgdGlsIEdyYW4gQ2FuYXJpYSwgRnVlcnRldmVudHVyYSBvZyBMYW56YXJvdGV8fHwx","orderDate":"2016-05-27T00:00:00+02:00","status":"In process","library":"DK-100451","holdQueuePosition":"1","author":"Loumann, Ole"}],"debt":[],"ddbcmsapi":"http://rest.filmstriben.dbc.inlead.dk/web/"}});
         done();
       });
   });

commit dec3986ddf8582fc002d9d03d692c0ff473a5dde

fix error introduced in #515

src/transformers/user.js
@@ -105,7 +105,7 @@ export default (request, context) => {
     
     let debts = [];
     if (body.data.getUserStatusResponse.userStatus.fiscalAccount) {
-      debts = body.data.getUserStatusResponse.userStatus.fiscalAccount.fiscalTransaction;
+      debts = body.data.getUserStatusResponse.userStatus.fiscalAccount.fiscalTransaction || [];
     }
 
     let data = {id: id.toString('base64'),