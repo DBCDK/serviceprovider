c4cad54afe725e1975a4a49a6df7573f99e12bf6
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-05-31 10:07:49 +0200

    lint

--- src/transformers/ddbcms.js
+++ src/transformers/ddbcms.js
@@ -5,7 +5,10 @@ export function ddbcms(params, context) {
   params.agency = context.get('user.agency');
   params.key = context.get('app.ddbcmsapipassword');
   if (!params.key || !context.get('services.ddbcmsapi')) {
-    throw `DDBCMS-API not available for the current library. Make sure that DDBCMS-API is running in the relevant DDBCMS deployment (agency: ${params.agency}), and that the ServiceProvider has been configured with the proper url and token.`;
+    throw 'DDBCMS-API not available for the current library. ' +
+      'Make sure that DDBCMS-API is running in the relevant DDBCMS deployment ' +
+      '(agency: ' + params.agency + '), ' +
+      'and that the ServiceProvider has been configured with the proper url and token.';
   }
   return context.query(context.get('services.ddbcmsapi') + 'content/fetch', params);
 }
