7dc9e774a29cb70193c16f44a8cb11821b3df355
cboesgaard <cbo@dbc.dk>
2016-06-08 10:59:21 +0200

    urls for zabbix (#539)

--- performancetest/zabbix-urls.sh
+++ performancetest/zabbix-urls.sh
@@ -1,106 +1,41 @@
-#!/bin/bash
+# this is examples of requests that can be used in zabbix
 
-export TIMES=1
-#export URL='http://xp-p02.dbc.dk:3000/v1/'
-export URL='@@URL'
-#export ACCESS_TOKEN='64141bea42b72443ce3848455f5960e1438fb434'
-# platformbe-p01
-export ACCESS_TOKEN="@@TOKEN"
-
-# local installation
-#export ACCESS_TOKEN='qwerty'
-export JOBS=1
-export PRE='ZABBIX'
+# you must edit these
+export URL='http://localhost:8080/v1/'
+export TOKEN="qwerty"
 
 
-export ENDPOINT='search'
-export REQUEST='{ "q": "hest", "fields": ["title"], "timings":true}'
-./make-test.sh
+curl -sSL -w "\nCURL HTTPCODE=%{http_code} SECS=%{time_total}\n" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -X GET -d '{"pid": "870970-basis:51989252", "timings":true}' "${URL}availability"
 
-export ENDPOINT='work'
-export REQUEST='{ "pids": ["870970-basis:22629344"], "fields": ["title" ], "timings":true}'
-export NAME='-simple'
-./make-test.sh
-export NAME=''
+curl -sSL -w "\nCURL HTTPCODE=%{http_code} SECS=%{time_total}\n" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -X GET -d '{"fields": ["title", "changed", "nid", "field_ding_event_title_image"], "timings":true}' "${URL}events"
 
-export ENDPOINT='work'
-export REQUEST='{ "pids": ["870970-basis:22629344"], "fields": ["title", "collection" ], "timings":true}'
-export NAME='-collection'
-./make-test.sh
-export NAME=''
+curl -sSL -w "\nCURL HTTPCODE=%{http_code} SECS=%{time_total}\n" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -X GET -d '{"q" : "harry", "timings":true}' "${URL}facets"
 
-export ENDPOINT='work'
-export REQUEST='{ "pids": ["870970-basis:22629344"], "fields": ["title", "coverUrlFull"], "timings":true}'
-export NAME='-coverurl'
-./make-test.sh
-export NAME=''
+curl -sSL -w "\nCURL HTTPCODE=%{http_code} SECS=%{time_total}\n" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -X GET -d '{"agencyIds": ["717500"], "fields": ["agencyId", "orderParameters"], "timings":true}' "${URL}libraries"
 
-export ENDPOINT='suggest'
-export REQUEST='{ "q": "hest", "type": "subject", "timings":true}'
-export NAME='-subject'
-./make-test.sh
-export NAME=''
+curl -sSL -w "\nCURL HTTPCODE=%{http_code} SECS=%{time_total}\n" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -X GET -d '{"fields": ["title", "changed", "nid", "field_ding_news_list_image"], "timings":true}' "${URL}news"
 
-export ENDPOINT='suggest'
-export REQUEST='{ "q": "hest", "type": "creator", "timings":true}'
-export NAME='-creator'
-./make-test.sh
-export NAME=''
+#curl -sSL -w "\nCURL HTTPCODE=%{http_code} SECS=%{time_total}\n" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -X GET -d '{"pids": ["870970-basis:28126727"], "expires": "2016-08-01", "library": "DK-100451", "timings":true}' "${URL}order"
 
-export ENDPOINT='suggest'
-export REQUEST='{ "q": "køge", "type": "library", "timings":true}'
-export NAME='-library'
-./make-test.sh
-export NAME=''
+curl -sSL -w "\nCURL HTTPCODE=%{http_code} SECS=%{time_total}\n" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -X GET -d '{"pids":["870970-basis:28511663", "870970-basis:29754519", "870970-basis:29060835", "870970-basis:28567057", "870970-basis:28043872", "870970-basis:28709994", "870970-basis:29319006", "870970-basis:27266428", "870970-basis:28724683", "874310-katalog:DBB0019369", "870970-basis:26923530", "870970-basis:29974861", "870970-basis:28456433", "870970-basis:28902239", "870970-basis:45188981"], "like":["870970-basis:45488713", "870970-basis:28643713", "870970-basis:29494940", "870970-basis:29386404", "870970-basis:28429576"], "timings":true}' "${URL}rank"
 
-export ENDPOINT='suggest'
-export REQUEST='{ "q": "harry", "type": "title", "timings":true}'
-export NAME='-title'
-./make-test.sh
-export NAME=''
+curl -sSL -w "\nCURL HTTPCODE=%{http_code} SECS=%{time_total}\n" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -X GET -d '{"like":["870970-basis:45488713", "870970-basis:28643713", "870970-basis:29494940", "870970-basis:29386404", "870970-basis:28429576"], "limit":10, "timings":true}' "${URL}recommend"
 
-export ENDPOINT='facets'
-export REQUEST='{"q" : "harry", "timings":true}'
-./make-test.sh
+#curl -sSL -w "\nCURL HTTPCODE=%{http_code} SECS=%{time_total}\n" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -X GET -d '{ "loanId": "NCIPMDAxOXw5OTAwfHw5OTAwLnx8", "timings":true}' "${URL}renew"
 
-export ENDPOINT='libraries'
-export REQUEST='{"agencyIds": ["717500"], "fields": ["agencyId", "orderParameters"], "timings":true}'
-./make-test.sh
+curl -sSL -w "\nCURL HTTPCODE=%{http_code} SECS=%{time_total}\n" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -X GET -d '{ "q": "hest", "fields": ["title"], "timings":true}' "${URL}search"
 
-export ENDPOINT='recommend'
-export REQUEST='{"like":["870970-basis:45488713", "870970-basis:28643713", "870970-basis:29494940", "870970-basis:29386404", "870970-basis:28429576"], "limit":10, "timings":true}'
-./make-test.sh
+curl -sSL -w "\nCURL HTTPCODE=%{http_code} SECS=%{time_total}\n" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -X GET -d '{ "q": "hest", "type": "creator", "timings":true}' "${URL}suggest"
 
-export ENDPOINT='rank'
-export REQUEST='{"pids":["870970-basis:28511663", "870970-basis:29754519", "870970-basis:29060835", "870970-basis:28567057", "870970-basis:28043872", "870970-basis:28709994", "870970-basis:29319006", "870970-basis:27266428", "870970-basis:28724683", "874310-katalog:DBB0019369", "870970-basis:26923530", "870970-basis:29974861", "870970-basis:28456433", "870970-basis:28902239", "870970-basis:45188981"], "like":["870970-basis:45488713", "870970-basis:28643713", "870970-basis:29494940", "870970-basis:29386404", "870970-basis:28429576"], "timings":true}'
-./make-test.sh
+curl -sSL -w "\nCURL HTTPCODE=%{http_code} SECS=%{time_total}\n" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -X GET -d '{ "q": "køge", "type": "library", "timings":true}' "${URL}suggest"
 
-export ENDPOINT='availability'
-export REQUEST='{"pid": "870970-basis:51989252", "timings":true}'
-./make-test.sh
+curl -sSL -w "\nCURL HTTPCODE=%{http_code} SECS=%{time_total}\n" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -X GET -d '{ "q": "hest", "type": "subject", "timings":true}' "${URL}suggest"
 
-export ENDPOINT='news'
-export REQUEST='{"fields": ["title", "changed", "nid", "field_ding_news_list_image"], "timings":true}'
-./make-test.sh
+curl -sSL -w "\nCURL HTTPCODE=%{http_code} SECS=%{time_total}\n" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -X GET -d '{ "q": "harry", "type": "title", "timings":true}' "${URL}suggest"
 
+curl -sSL -w "\nCURL HTTPCODE=%{http_code} SECS=%{time_total}\n" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -X GET -d '{ "pids": ["870970-basis:22629344"], "fields": ["title", "collection" ], "timings":true}' "${URL}work"
 
-export ENDPOINT='events'
-export REQUEST='{"fields": ["title", "changed", "nid", "field_ding_event_title_image"], "timings":true}'
-./make-test.sh
+curl -sSL -w "\nCURL HTTPCODE=%{http_code} SECS=%{time_total}\n" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -X GET -d '{ "pids": ["870970-basis:22629344"], "fields": ["title", "coverUrlFull"], "timings":true}' "${URL}work"
 
-export ENDPOINT='order'
-export REQUEST='{"pids": ["870970-basis:28126727"], "expires": "2016-08-01", "library": "DK-100451", "timings":true}'
-./make-test.sh
-
-export ENDPOINT='renew'
-export REQUEST='{ "loanId": "NCIPMDAxOXw5OTAwfHw5OTAwLnx8", "timings":true}'
-./make-test.sh
-
-
-# export ENDPOINT='work'
-# export REQUEST='{ "pids":["870970-basis:28511663", "870970-basis:29754519", "870970-basis:29060835", "870970-basis:28567057", "870970-basis:28043872", "870970-basis:28709994", "870970-basis:29319006", "870970-basis:27266428", "870970-basis:28724683", "870970-basis:26923530", "870970-basis:29974861", "870970-basis:28456433", "870970-basis:28902239", "870970-basis:45188981", "870970-basis:45488713", "870970-basis:28643713", "870970-basis:29494940", "870970-basis:29386404", "870970-basis:28429576"], "fields": ["title", "coverUrlFull"], "timings":true}'
-# export NAME='-mucho-coverurl'
-# ./make-test.sh
-# export NAME=''
-
+curl -sSL -w "\nCURL HTTPCODE=%{http_code} SECS=%{time_total}\n" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -X GET -d '{ "pids": ["870970-basis:22629344"], "fields": ["title" ], "timings":true}' "${URL}work"
 
