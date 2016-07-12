5a43b3cee7294a335c606b72b863e381e4cd2aa3
Martin Møller <mmj@dbc.dk>
2016-07-12 15:17:53 +0200

    Changes made based on SonarQube

--- src/main.js
+++ src/main.js
@@ -25,12 +25,13 @@ if (missingEnvVars.length > 0) {
   process.exit(1);
 }
 
-new SocketCluster({ // eslint-disable-line no-new
+module.exports = new SocketCluster({ // eslint-disable-line no-new
   workers: Number(process.env.NODE_WEB_WORKERS) || 1, // eslint-disable-line no-process-env
   brokers: Number(process.env.NODE_WEB_BROKERS) || 1, // eslint-disable-line no-process-env
   port: Number(process.env.PORT) || 8080, // eslint-disable-line no-process-env
   path: '/v' + majorVersion + '/socketcluster/',
   initController: path.join(__dirname, 'init.js'),
   workerController: path.join(__dirname, 'app.js'),
+  wsEngine: 'uws',
   rebootWorkerOnCrash: process.env.AUTO_REBOOT || true // eslint-disable-line no-process-env
 });
