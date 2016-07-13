a3a66d6caf17ea797848bcda9760c12cda5eb017
Martin Møller <mmj@dbc.dk>
2016-07-13 11:39:25 +0200

    Reducing unit size in facets.js

--- src/transformers/facets.js
+++ src/transformers/facets.js
@@ -2,18 +2,12 @@
 
 import {log} from '../utils.js';
 
-export default (params, context) => new Promise((resolve) => {
-  if (!params.q) {
-    return resolve({statusCode: 400,
-                    error: 'missing q parameter'});
-  }
+function generateSoap(params, context) {
+  const facets = (Array.isArray(params.fields) && params.fields) || ['creator', 'subject', 'language', 'date', 'form'];
+  const agency = context.get('search.agency');
+  const profile = context.get('search.profile');
 
-  let facets = (Array.isArray(params.fields) && params.fields) ||
-                  ['creator', 'subject', 'language', 'date', 'form'];
-  let agency = context.get('search.agency');
-  let profile = context.get('search.profile');
-  
-  let soap = `<?xml version="1.0" encoding="UTF-8"?>
+  return `<?xml version="1.0" encoding="UTF-8"?>
 <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
     xmlns:open="http://oss.dbc.dk/ns/opensearch">
   <SOAP-ENV:Body>
@@ -26,8 +20,8 @@ export default (params, context) => new Promise((resolve) => {
         <open:facetSort>count</open:facetSort>
         <open:facetMinCount>1</open:facetMinCount>
         ${facets.map(s =>
-          '<open:facetName>facet.' + String(s).replace(/</g, '&lt;') + '</open:facetName>').join('\n        ')
-        }
+  '<open:facetName>facet.' + String(s).replace(/</g, '&lt;') + '</open:facetName>').join('\n        ')
+    }
       </open:facets>
       <open:start>1</open:start>
       <open:stepValue>0</open:stepValue>
@@ -35,10 +29,21 @@ export default (params, context) => new Promise((resolve) => {
     </open:searchRequest>
   </SOAP-ENV:Body>
 </SOAP-ENV:Envelope>`;
+}
+
+export default (params, context) => new Promise((resolve) => {
+  if (!params.q) {
+    return resolve({
+      statusCode: 400,
+      error: 'missing q parameter'
+    });
+  }
+
+  const soap = generateSoap(params, context);
 
   context.call('opensearch', soap).then(body => {
     try {
-      let result = {};
+      const result = {};
       body = JSON.parse(body).searchResponse;
 
       if (body.error) {
@@ -48,18 +53,30 @@ export default (params, context) => new Promise((resolve) => {
         });
       }
 
-      facets = ((body.result.facetResult || {}).facet || []);
+      const facets = ((body.result.facetResult || {}).facet || []);
       facets.forEach(facet => {
-        let name = facet.facetName.$.replace('facet.', '');
+        const name = facet.facetName.$.replace('facet.', '');
         if (Array.isArray(facet.facetTerm)) {
-          result[name] = facet.facetTerm.map(o => ({term: o.term.$, frequency: parseInt(o.frequence.$, 10)}));
+          result[name] = facet.facetTerm.map(o => ({
+            term: o.term.$,
+            frequency: parseInt(o.frequence.$, 10)
+          }));
         }
       });
-      resolve({statusCode: 200, data: result});
+      resolve({
+        statusCode: 200,
+        data: result
+      });
     }
     catch (e) {
-      log.error('response from facet-request', {error: e, errorString: String(e)});
-      resolve({statusCode: 400, error: 'error fulfilling the request'});
+      log.error('response from facet-request', {
+        error: e,
+        errorString: String(e)
+      });
+      resolve({
+        statusCode: 400,
+        error: 'error fulfilling the request'
+      });
     }
   });
 });
