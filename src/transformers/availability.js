a6126d16f90ca7c5d68b617fb9cd2e764889780e
Jonas Peter Hyatt <jph@dbc.dk>
2016-07-15 14:21:42 +0200

    better coverage...

--- src/transformers/availability.js
+++ src/transformers/availability.js
@@ -7,18 +7,15 @@ import _ from 'lodash';
 
 export default (request, context) => {
 
-  let promises = [];
-  promises.push(openHoldingStatus(request, context));
-  promises.push(getOrderPolicy({pids: [request.pid]}, context));
+  const promises = [
+    openHoldingStatus(request, context),
+    getOrderPolicy({pids: [request.pid]}, context)
+  ];
 
   return Promise.all(promises).then(body => {
-    if (body.length !== 2) {
-      log.error('Illegal number of returned promises. Should be 2, but was ' + body.length);
-      return {statusCode: 500, error: 'Internal Server Error'};
-    }
     // We now know there are two returned promises in body!
-    let openHoldingStatusRes = body[0];
-    let getOrderPolicyRes = body[1];
+    const openHoldingStatusRes = body[0];
+    const getOrderPolicyRes = body[1];
 
     if (!_.has(openHoldingStatusRes, 'data.willLend')) {
       return {statusCode: 200, data: {willLend: false}};
@@ -30,6 +27,5 @@ export default (request, context) => {
     }
 
     return openHoldingStatusRes;
-
   });
 };
