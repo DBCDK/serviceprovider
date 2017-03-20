8fa356343c91f22ff8ba00a84d73ef481fc539fd
Vibjerg <vibjerg@gmail.com>
2017-03-20 11:33:29 +0100

    Add context

--- src/transformers/community/community.js
+++ src/transformers/community/community.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
 import profile from './profile';
 
+import {accessLogMiddleware, getContextMiddleware, requireAuthorized} from '../../app.middlewares';
+
+
+/**
+ * Router for all community/profile endpoints.
+ *
+ * @returns {*}
+ */
 export default () => {
+  router.use(getContextMiddleware);
+  router.use(requireAuthorized);
   router.use('/profile', profile());
   return router;
 };
