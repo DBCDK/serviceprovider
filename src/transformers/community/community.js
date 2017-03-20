aaad86c509fa840823ca624f4a09331142acdece
Vibjerg <vibjerg@gmail.com>
2017-03-20 19:45:17 +0100

    Make call to community service

--- src/transformers/community/community.js
+++ src/transformers/community/community.js
@@ -1,8 +1,26 @@
 const router = require('express').Router();
 import profile from './profile';
+import caller from '../../provider/caller';
+import request from 'request';
 
 import {accessLogMiddleware, getContextMiddleware, requireAuthorized} from '../../app.middlewares';
 
+function getCommunityClient(req, res, next) {
+  const context = caller({}, req.context);
+  const url = context.get('services.communityservice') || 'http://localhost:3000/v1';
+  const id = context.get('communityservice.id') || 1;
+  req.communityReguest = (path, method, params) => {
+    const options = Object.assign({
+      url: `${url}/community/${id}/${path}`,
+      method
+    }, params);
+    console.log(options);
+    return new Promise((resolve, reject) => {
+      request(options, (error, response, data) => error ? reject(error) : resolve(data));
+    });
+  };
+  return next();
+}
 
 /**
  * Router for all community/profile endpoints.
@@ -11,6 +29,7 @@ import {accessLogMiddleware, getContextMiddleware, requireAuthorized} from '../.
  */
 export default () => {
   router.use(getContextMiddleware);
+  router.use(getCommunityClient);
   router.use(requireAuthorized);
   router.use('/profile', profile());
   return router;
--- src/transformers/community/profile.js
+++ src/transformers/community/profile.js
@@ -4,15 +4,14 @@ export default () => {
   return router;
 };
 
-
 router.get('/', (req, res) => {
   // TODO implement list method.
 });
 
 router.get('/:userId', (req, res) => {
-  res.jsonp({
-    profile: {
-      id: req.params.userId
-    }
-  });
+  req.communityReguest(`profile/${req.params.userId}`, 'get').then(result => {
+    res.jsonp(JSON.parse(result));
+  }).catch(
+    err => res.jsonp(err)
+  );
 });
