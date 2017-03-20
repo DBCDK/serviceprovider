000761b2c8b198bcf93d5ac2486445dfb97100af
Vibjerg <vibjerg@gmail.com>
2017-03-20 11:33:41 +0100

    dummy profile response

--- src/transformers/community/profile.js
+++ src/transformers/community/profile.js
@@ -3,3 +3,16 @@ const router = require('express').Router();
 export default () => {
   return router;
 };
+
+
+router.get('/', (req, res) => {
+  // TODO implement list method.
+});
+
+router.get('/:userId', (req, res) => {
+  res.jsonp({
+    profile: {
+      id: req.params.userId
+    }
+  });
+});
