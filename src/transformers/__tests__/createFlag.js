ffabe0f2793e32b9040dae15f3258ba0792405d1
Vibjerg <vibjerg@gmail.com>
2017-05-15 14:57:02 +0200

    update test

--- src/transformers/__tests__/createFlag.js
+++ src/transformers/__tests__/createFlag.js
@@ -1,12 +1,12 @@
 /* eslint-disable max-len, quotes, comma-spacing, key-spacing, quote-props, indent */
-// Request: createEntity {"reason":"this is the reason","profile_id":1,"reference":{"type":"post","id":5},"_meta":{"type":"flag","elvisType":"action","schemaMap":{"reason":"attributes.reason","reference":"attributes.reference","profile_id":"owner_id","created_epoch":"created_epoch","deleted_epoch":"deleted_epoch","modified_epoch":"modified_epoch","modified_by":"modified_by","id":"id"},"schema":{"properties":{"reason":{"type":"string"},"reference":{"type":"object","properties":{"type":{"type":"string","required":true},"id":{"type":"integer","required":true}}},"profile_id":{"type":"number","format":"integer"},"created_epoch":{"type":"integer"},"deleted_epoch":{"type":"integer"},"modified_epoch":{"type":"integer"},"modified_by":{"type":"integer"},"id":{"type":"integer"}}}}}
+// Request: createEntity {"reference":{"type":"post","id":6465},"reason":"this is the reason","profile_id":1,"_meta":{"type":"flag","elvisType":"action","schemaMap":{"reason":"attributes.reason","reference":"attributes.reference","profile_id":"owner_id","created_epoch":"created_epoch","deleted_epoch":"deleted_epoch","modified_epoch":"modified_epoch","modified_by":"modified_by","id":"id"},"schema":{"properties":{"reason":{"type":"string"},"reference":{"type":"object","required":["type","id"],"properties":{"type":{"type":"string"},"id":{"type":"integer"}}},"profile_id":{"type":"number","format":"integer"},"created_epoch":{"type":"integer"},"deleted_epoch":{"type":"integer"},"modified_epoch":{"type":"integer"},"modified_by":{"type":"integer"},"id":{"type":"integer"}}}}}
 
 import Provider from '../../provider/Provider.js';
 import {assert, fail} from 'chai';
 
 const context = {
   "services": {
-    "ddbcmsapi": "http://rest.filmstriben.dbc.inlead.dk/web/",
+    "ddbcmsapi": "https://cmscontent.dbc.dk/",
     "moreinfo": "http://moreinfo.addi.dk/2.6/",
     "openagency": "http://openagency.addi.dk/2.24/",
     "openholdingstatus": "https://openholdingstatus.addi.dk/2.2/",
@@ -23,10 +23,10 @@ const context = {
       "default": "https://xptest.dbc.dk/ms/recommend-cosim/v1",
       "popular": "https://xptest.dbc.dk/ms/recommend-pop/v1"
     },
-    "communityservice": "http://localhost:3001/v1"
+    "communityservice": "http://localhost:4010/v1"
   },
   "communityservice": {
-    "id": 1
+    "id": "1"
   },
   "search": {
     "agency": "775100",
@@ -54,28 +54,33 @@ const context = {
 };
 const provider = Provider();
 const mockData = {
-  "[\"http://localhost:3001/v1/community/1/action\",{\"method\":\"post\",\"json\":{\"attributes\":{\"reason\":\"this is the reason\",\"reference\":{\"type\":\"post\",\"id\":5}},\"owner_id\":1,\"entity_ref\":5,\"type\":\"flag\"}}]": {
+  "[\"http://localhost:4010/v1/community/1/query\",{\"method\":\"post\",\"json\":{\"Entity\":{\"id\":6465,\"type\":\"post\"},\"Include\":{\"id\":\"id\"}}}]": {
+    "data": {
+      "id": 6465
+    }
+  },
+  "[\"http://localhost:4010/v1/community/1/action\",{\"method\":\"post\",\"json\":{\"attributes\":{\"reason\":\"this is the reason\",\"reference\":{\"type\":\"post\",\"id\":6465}},\"owner_id\":1,\"entity_ref\":6465,\"type\":\"flag\"}}]": {
     "links": {
-      "self": "/v1/community/1/action/16"
+      "self": "/v1/community/1/action/3897"
     },
     "data": {
-      "id": 16,
-      "created_epoch": 1493715177,
+      "id": 3897,
+      "created_epoch": 1494852864,
       "deleted_epoch": null,
-      "modified_epoch": 1493715177,
+      "modified_epoch": 1494852864,
       "modified_by": null,
       "deleted_by": null,
       "community_id": 1,
       "owner_id": 1,
       "start_epoch": null,
       "end_epoch": null,
-      "entity_ref": 5,
+      "entity_ref": 6465,
       "profile_ref": null,
       "type": "flag",
       "attributes": {
         "reason": "this is the reason",
         "reference": {
-          "id": 5,
+          "id": 6465,
           "type": "post"
         }
       },
@@ -85,9 +90,9 @@ const mockData = {
 };
 
 describe('Automated test: createFlag', () => {
-  it('expected response. ID:ff9jmy, for {"reason":"this is the reason","profile_id":1,"reference":{"type":"post","id":5},"_meta":{"type":"flag","elvisType":"action","schemaMap":{"reason":"attributes.reason","reference":"attributes.reference","profile_id":"owner_id","created_epoch":"created_epoch","deleted_epoch":"deleted_epoch","modified_epoch":"modified_epoch","modified_by":"modified_by","id":"id"},"schema":{"properties":{"reason":{"type":"string"},"reference":{"type":"object","properties":{"type":{"type":"string","required":true},"id":{"type":"integer","required":true}}},"profile_id":{"type":"number","format":"integer"},"created_epoch":{"type":"integer"},"deleted_epoch":{"type":"integer"},"modified_epoch":{"type":"integer"},"modified_by":{"type":"integer"},"id":{"type":"integer"}}}}}', (done) => {
+  it('expected response. ID:p8ph76, for {"reference":{"type":"post","id":6465},"reason":"this is the reason","profile_id":1,"_meta":{"type":"flag","elvisType":"action","schemaMap":{"reason":"attributes.reason","reference":"attributes.reference","profile_id":"owner_id","created_epoch":"created_epoch","deleted_epoch":"deleted_epoch","modified_epoch":"modified_epoch","modified_by":"modified_by","id":"id"},"schema":{"properties":{"reason":{"type":"string"},"reference":{"type":"object","required":["type","id"],"properties":{"type":{"type":"string"},"id":{"type":"integer"}}},"profile_id":{"type":"number","format":"integer"},"created_epoch":{"type":"integer"},"deleted_epoch":{"type":"integer"},"modified_epoch":{"type":"integer"},"modified_by":{"type":"integer"},"id":{"type":"integer"}}}}}', (done) => {
     context.mockData = mockData;
-    provider.execute('createEntity', {"reason":"this is the reason","profile_id":1,"reference":{"type":"post","id":5},"_meta":{"type":"flag","elvisType":"action","schemaMap":{"reason":"attributes.reason","reference":"attributes.reference","profile_id":"owner_id","created_epoch":"created_epoch","deleted_epoch":"deleted_epoch","modified_epoch":"modified_epoch","modified_by":"modified_by","id":"id"},"schema":{"properties":{"reason":{"type":"string"},"reference":{"type":"object","properties":{"type":{"type":"string","required":true},"id":{"type":"integer","required":true}}},"profile_id":{"type":"number","format":"integer"},"created_epoch":{"type":"integer"},"deleted_epoch":{"type":"integer"},"modified_epoch":{"type":"integer"},"modified_by":{"type":"integer"},"id":{"type":"integer"}}}}}, context)
+    provider.execute('createEntity', {"reference":{"type":"post","id":6465},"reason":"this is the reason","profile_id":1,"_meta":{"type":"flag","elvisType":"action","schemaMap":{"reason":"attributes.reason","reference":"attributes.reference","profile_id":"owner_id","created_epoch":"created_epoch","deleted_epoch":"deleted_epoch","modified_epoch":"modified_epoch","modified_by":"modified_by","id":"id"},"schema":{"properties":{"reason":{"type":"string"},"reference":{"type":"object","required":["type","id"],"properties":{"type":{"type":"string"},"id":{"type":"integer"}}},"profile_id":{"type":"number","format":"integer"},"created_epoch":{"type":"integer"},"deleted_epoch":{"type":"integer"},"modified_epoch":{"type":"integer"},"modified_by":{"type":"integer"},"id":{"type":"integer"}}}}}, context)
       .then(result => {
         assert.deepEqual(result,
             {
@@ -95,13 +100,13 @@ describe('Automated test: createFlag', () => {
   "data": {
     "reason": "this is the reason",
     "reference": {
-      "id": 5,
+      "id": 6465,
       "type": "post"
     },
     "profile_id": 1,
-    "created_epoch": 1493715177,
-    "modified_epoch": 1493715177,
-    "id": 16
+    "created_epoch": 1494852864,
+    "modified_epoch": 1494852864,
+    "id": 3897
   },
   "errors": []
 });
@@ -113,13 +118,13 @@ describe('Automated test: createFlag', () => {
   "data": {
     "reason": "this is the reason",
     "reference": {
-      "id": 5,
+      "id": 6465,
       "type": "post"
     },
     "profile_id": 1,
-    "created_epoch": 1493715177,
-    "modified_epoch": 1493715177,
-    "id": 16
+    "created_epoch": 1494852864,
+    "modified_epoch": 1494852864,
+    "id": 3897
   },
   "errors": []
 });
