d79bce28ae0d9b25d2304aa14fd531b908276e5e
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2018-04-18 10:26:07 +0200

    update client library, #886

--- client/dbc_openplatform.js
+++ client/dbc_openplatform.js
@@ -1,42 +1,47 @@
-(function() {
-  if (typeof Promise === 'undefined') {
-    throw 'old browser without Promise object. Please load a polyfill before loading dbc_openplatform.min.js, if you want to make it work';
-  }
-  var sc;
-  sc = false;
-  var apiToken;
-  var endpoints = [
-    'availability',
-    'events',
-    'facets',
-    'libraries',
-    'news',
-    'order',
-    'rank',
-    'recommend',
-    'renew',
-    'search',
-    'suggest',
-    'user',
-    'work'];
+if (typeof Promise === 'undefined') {
+  throw 'old browser without Promise object. Please load a polyfill before loading dbc_openplatform.min.js, if you want to make it work';
+}
 
-  dbcOpenPlatform = {};
-  /**
-   * Create a new method, for an endpoint.
-   * The resulting function takes (an object of parameters) as parameter, 
-   * and returns a promise with the result.
-   */
-  function endpoint(name) {
-    return function(obj) {
+var sc;
+sc = false;
+var apiToken;
+var endpoints = [
+  'availability',
+  'events',
+  'facets',
+  'libraries',
+  'news',
+  'order',
+  'recommend',
+  'renew',
+  'search',
+  'status',
+  'suggest',
+  'user',
+  'work'
+];
+
+var connectedFn;
+var connecting = new Promise(function(resolve, reject) {
+  connectedFn = resolve;
+});
+
+openplatform = {};
+/**
+ * Create a new method, for an endpoint.
+ * The resulting function takes (an object of parameters) as parameter,
+ * and returns a promise with the result.
+ */
+function endpoint(name) {
+  return function(obj) {
+    return connecting.then(function() {
       var params = {access_token: apiToken};
-      for (var key in obj) if (obj.hasOwnProperty(key)) {
-        params[key] = obj[key];
-      }
+      for (var key in obj)
+        if (obj.hasOwnProperty(key)) {
+          params[key] = obj[key];
+        }
       var envelope = params.envelope;
       delete params.envelope;
-      if (!this.connected()) {
-        return Promise.reject('need to connect before calling endpoint');
-      }
       return new Promise(function(resolve, reject) {
         sc.emit(name, params, function(err, result) {
           if (err) {
@@ -49,133 +54,136 @@
           }
         });
       });
-    };
-  }
+    });
+  };
+}
 
-  /**
-   * Assign methods for all endpoints.
-   */
-  for (var i = 0; i < endpoints.length; ++i) {
-    dbcOpenPlatform[endpoints[i]] = endpoint(endpoints[i]);
-  }
+/**
+ * Assign methods for all endpoints.
+ */
+for (var i = 0; i < endpoints.length; ++i) {
+  openplatform[endpoints[i]] = endpoint(endpoints[i]);
+}
 
-  /**
-   * Connect to the DBC open platform.
-   * takes token, or client_id,client_secret 
-   * or client_id,client_secret,user_id,user_passed
-   * as parameters.
-   *
-   * @return a promise with the token.
-   */
-  dbcOpenPlatform.connect = function() {
-    var promise, args, clientId, clientSecret, user, password;
+/**
+ * Connect to the DBC open platform.
+ * takes token, or client_id,client_secret
+ * or client_id,client_secret,user_id,user_passed
+ * as parameters.
+ *
+ * @return a promise with the token.
+ */
+openplatform.connect = function() {
+  var promise, args, clientId, clientSecret, user, password;
 
-    args = Array.prototype.slice.call(arguments);
-    if (!args.length === 0 && !apiToken) {
-      if (!apiToken) {
-        throw 'missing token to connect';
-      }
-      promise = Promise.resolve(apiToken);
-    } else if (args.length === 1) {
-      promise = Promise.resolve(args[0]);
-    } else {
-      clientId = args[0];
-      clientSecret = args[1];
-      if (args.length === 2) {
-        user = '@';
-        password = '@';
-      } else if (args.length === 4) {
-        user = args[2];
-        password = args[3];
-      } else {
-        throw 'dbcOpenPlatform.connect takes 0, 1, 2 or 4 parameters';
-      }
-      if (user.indexOf('@') === -1) {
-        throw 'id should be the concatenation of user-id, "@", and agency-id';
-      }
-      promise = getToken(clientId, clientSecret, user, password);
+  args = Array.prototype.slice.call(arguments);
+  if (!args.length === 0 && !apiToken) {
+    if (!apiToken) {
+      throw 'missing token to connect';
     }
-    return promise.then(function(token) {
-      apiToken = token;
-      return new Promise(function(resolve, reject) {
-        if (sc) {
-          if (dbcOpenPlatform.connected()) {
-            return resolve(token);
-          }
-          sc.on('connectAbort', function() {
-            sc = undefined;
-            resolve(dbcOpenPlatform.connect(token));
-          });
-          sc.on('connect', function() {
-            resolve(token);
-          });
-          sc.connect();
-          return;
-        } 
-        sc = require('socketcluster-client').connect({
-          hostname: 'openplatform.dbc.dk', 
-          port: 443,
-          secure: true,
-          path: '/v1/socketcluster/?access_token=' + token
-        });
-        sc.on('connectAbort', function(result) {
-          reject(result);
+    promise = Promise.resolve(apiToken);
+  } else if (args.length === 1) {
+    promise = Promise.resolve(args[0]);
+  } else {
+    clientId = args[0];
+    clientSecret = args[1];
+    if (args.length === 2) {
+      user = '@';
+      password = '@';
+    } else if (args.length === 4) {
+      user = args[2];
+      password = args[3];
+    } else {
+      throw 'openplatform.connect takes 0, 1, 2 or 4 parameters';
+    }
+    if (user.indexOf('@') === -1) {
+      throw 'id should be the concatenation of user-id, "@", and agency-id';
+    }
+    promise = getToken(clientId, clientSecret, user, password);
+  }
+  return promise.then(function(token) {
+    apiToken = token;
+    return new Promise(function(resolve, reject) {
+      if (sc) {
+        if (openplatform.connected()) {
+          return resolve(token);
+        }
+        sc.on('connectAbort', function() {
+          sc = undefined;
+          resolve(openplatform.connect(token));
         });
         sc.on('connect', function() {
           resolve(token);
         });
+        sc.connect();
+        return;
+      }
+      sc = require('socketcluster-client').connect({
+        hostname: 'openplatform.dbc.dk',
+        ackTimeout: 30000,
+        port: 443,
+        secure: true,
+        path: '/v3/socketcluster/?access_token=' + token
+      });
+      sc.on('connectAbort', function(result) {
+        reject(result);
+      });
+      sc.on('connect', function() {
+        connectedFn();
+        resolve(token);
       });
     });
-  };
-  /**
-   * Disconnect from server
-   *
-   * @return a promise of nothing.
-   */
-  dbcOpenPlatform.disconnect = function() {
-    if (sc) {
-      sc.disconnect();
-    }
-    return Promise.resolve();
-  };
-  /**
-   * Check if we are connected to the server
-   * @return a boolean of whether we are connected
-   */
-  dbcOpenPlatform.connected = function() {
-    return sc && sc.state === 'open';
-  };
-  /**
-   * Get a token, given a login.
-   * This function is not exported.
-   */
-  function getToken(clientId, clientSecret, user, passwd) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.open('POST', 'https://auth.dbc.dk/oauth/token');
-
-    xhr.setRequestHeader('Authorization', 'Basic ' + 
-        btoa(clientId + ':' + clientSecret));
-
-    xhr.setRequestHeader('Content-Type',
-        'application/x-www-form-urlencoded');
-
-    xhr.send('grant_type=password' +
-        '&username=' + user +
-        '&password=' + passwd);
-
-    return new Promise(function(resolve, reject) {
-      xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            var result = xhr.response;
-            result = JSON.parse(result).access_token;
-            resolve(result);
-          } else {
-            reject({statusCode: xhr.status, error: xhr.response});
-          }
-        }
-      };
-    });
+  });
+};
+/**
+ * Disconnect from server
+ *
+ * @return a promise of nothing.
+ */
+openplatform.disconnect = function() {
+  if (sc) {
+    sc.disconnect();
   }
-})();
+  return Promise.resolve();
+};
+/**
+ * Check if we are connected to the server
+ * @return a boolean of whether we are connected
+ */
+openplatform.connected = function() {
+  return sc && sc.state === 'open';
+};
+/**
+ * Get a token, given a login.
+ * This function is not exported.
+ */
+function getToken(clientId, clientSecret, user, passwd) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.open('POST', 'https://auth.dbc.dk/oauth/token');
+
+  xhr.setRequestHeader(
+    'Authorization',
+    'Basic ' + btoa(clientId + ':' + clientSecret)
+  );
+
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+  xhr.send('grant_type=password' + '&username=' + user + '&password=' + passwd);
+
+  return new Promise(function(resolve, reject) {
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          var result = xhr.response;
+          result = JSON.parse(result).access_token;
+          resolve(result);
+        } else {
+          reject({statusCode: xhr.status, error: xhr.response});
+        }
+      }
+    };
+  });
+}
+
+module.exports = openplatform;
