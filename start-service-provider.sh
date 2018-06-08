1812677500f5567587f049ecbe52d2d2471052ef
Emil Bay <github@tixz.dk>
2018-06-08 16:42:16 +0200

    Wait for child processes
    Now killing the script will also kill both child processes, not leaving any orphans behind

--- start-service-provider.sh
+++ start-service-provider.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 PORT=3000 node src/smaug/minismaug.js -f context-sample.json & sleep 2
-SMAUG=http://localhost:3000 MOCK_FILE=apitest/mockdata.json SWAGGER_HTTP=true TEST_DEV=true node src/main.js & sleep 10
+SMAUG=http://localhost:3000 MOCK_FILE=apitest/mockdata.json SWAGGER_HTTP=true TEST_DEV=true node src/main.js
 
+wait
