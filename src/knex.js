a74f0011a8446a21eac59e6764c93d4e235c0f09
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2018-10-04 10:10:52 +0200

    limit knex to a single connection, to see if this may be the cause of #992

--- src/knex.js
+++ src/knex.js
@@ -1,7 +1,8 @@
 const config = process.env.PG_CONNECTION_STRING
   ? {
       client: 'pg',
-      connection: process.env.PG_CONNECTION_STRING
+      connection: process.env.PG_CONNECTION_STRING,
+      pool: {min: 1, max: 1}
     }
   : {
       client: 'sqlite3',
