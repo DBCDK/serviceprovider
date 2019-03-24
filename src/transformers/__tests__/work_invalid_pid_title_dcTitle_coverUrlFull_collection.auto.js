c24159f7ffca3936f6cd0cdef6a95e3670ff23ab
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2019-03-24 12:57:02 +0100

    postpone regeneration of snapshot tests

--- src/transformers/__tests__/work_invalid_pid_title_dcTitle_coverUrlFull_collection.auto.js
+++ src/transformers/__tests__/work_invalid_pid_title_dcTitle_coverUrlFull_collection.auto.js
@@ -69,7 +69,7 @@ const provider = Provider();
 describe('Automated test: work_invalid_pid_title_dcTitle_coverUrlFull_collection.auto', () => {
   it('has same result as recorded (in work_invalid_pid_title_dcTitle_coverUrlFull_collection.auto)', () => {
     assert(
-      Date.now() < +new Date('2019-02-28'),
+      Date.now() < +new Date('2019-05-01'),
       'Please recreate the automatically generated unit tests, such that the mock data does not come out of sync with the actual services. See README.md for details.'
     );
     context.mockData = mockData;
