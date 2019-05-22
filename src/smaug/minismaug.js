81945c2f8cd479ce8304431ab442e76768b6a4c2
Kasper F <kfu@dbc.dk>
2019-05-22 10:52:06 +0200

    added minismaug functionality for specific token to configuration mappings

--- src/smaug/minismaug.js
+++ src/smaug/minismaug.js
@@ -1,11 +1,13 @@
 require('babel-register');
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var fs = require('fs');
 var log = require('../utils').log;
 var minimist = require('minimist');
 
 const app = express();
+app.use(bodyParser.json());
 
 const args = minimist(process.argv.slice(2));
 
@@ -18,8 +20,20 @@ const config = JSON.parse(fs.readFileSync(args.f, 'utf8'));
 
 var port = process.env.PORT || 3333; // eslint-disable-line no-process-env
 
+const configurations = {};
+
 app.get('/configuration', (req, res) => {
-  res.send(JSON.stringify(config.context, null, 2));
+  let c = configurations[req.query.token] || {};
+  res.send(JSON.stringify({...config.context, ...c}, null, 2));
+});
+
+app.put('/configuration', (req, res) => {
+  if (!req.query.token) {
+    throw new Error('Missing token');
+  }
+  configurations[req.query.token] = req.body;
+
+  res.send('OK');
 });
 
 app.listen(port, () => {
