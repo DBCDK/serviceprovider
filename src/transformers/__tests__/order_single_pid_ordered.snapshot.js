25d099b26eefd81498a8fc0ab1ae7a34d69c1373
mtni <mtni@dbc.dk>
2019-09-23 13:29:00 +0200

    changed expiration date

--- src/transformers/__tests__/order_single_pid_ordered.snapshot.js
+++ src/transformers/__tests__/order_single_pid_ordered.snapshot.js
@@ -63,7 +63,7 @@ const provider = Provider();
 describe('Automated test: order_single_pid_ordered.snapshot', () => {
   it('has same result as recorded (in order_single_pid_ordered.snapshot)', () => {
     assert(
-      Date.now() < +new Date('2019-09-17'),
+      Date.now() < +new Date('2020-01-17'),
       'Please recreate the automatically generated unit tests, such that the mock data does not come out of sync with the actual services. See README.md for details.'
     );
     context.mockData = mockData;
