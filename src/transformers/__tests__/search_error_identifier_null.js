fa6a3152528a4aec793168e54d90d7da8ce1a2d0
Steen Lillethorup Frederiksen <slf@dbc.dk>
2020-01-21 13:34:53 +0100

    Update date for snapshot tests

--- src/transformers/__tests__/search_error_identifier_null.js
+++ src/transformers/__tests__/search_error_identifier_null.js
@@ -65,7 +65,7 @@ const provider = Provider();
 describe('Automated test: search_error_identifier_null', () => {
   it('has same result as recorded (in search_error_identifier_null)', () => {
     assert(
-      Date.now() < +new Date('2020-01-17'),
+      Date.now() < +new Date('2021-01-17'),
       'Please recreate the automatically generated unit tests, such that the mock data does not come out of sync with the actual services. See README.md for details.'
     );
     context.mockData = mockData;
