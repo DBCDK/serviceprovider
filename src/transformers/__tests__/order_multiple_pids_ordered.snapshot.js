19c4a3fd6b0f0a08612a0a885ce9b82973b23393
pjo <pjo@dbc.dk>
2020-12-08 10:47:12 +0100

    updated a test

--- src/transformers/__tests__/order_multiple_pids_ordered.snapshot.js
+++ src/transformers/__tests__/order_multiple_pids_ordered.snapshot.js
@@ -1,4 +1,4 @@
-// AUTOTEST GENERATOR: {"endpoint":"order","params":{"pids":["870970-basis:28126727","870970-basis:27597726"],"expires":"2020-12-01","pickUpBranch":"DK-100450","phone":"123454","address":"ADDRESS","email":"EMAIL"}}
+// AUTOTEST GENERATOR: {"endpoint":"order","params":{"pids":["870970-basis:28126727","870970-basis:27597726"],"pickUpBranch":"DK-790900","expires":"2021-06-01"}}
 //
 //
 // AUTOMATED UNIT TEST
@@ -8,67 +8,77 @@
 const endpoint = 'order';
 const params = {
   pids: ['870970-basis:28126727', '870970-basis:27597726'],
-  expires: '2021-06-01',
-  pickUpBranch: 'DK-100450',
-  phone: '123454',
-  address: 'ADDRESS',
-  email: 'EMAIL'
+  pickUpBranch: 'DK-790900',
+  expires: '2021-06-01'
 };
 
 const expected = {
   statusCode: 200,
-  data: {status: 'ok', orsId: '1033955652'}
+  data: { status: 'ok', orsId: '1039377316' }
 };
 
 const context = {
   services: {
-    ddbcmsapi: 'http://rest.filmstriben.dbc.inlead.dk/web/',
-    moreinfo: 'http://moreinfo.addi.dk/2.11/',
-    openagency: 'http://openagency.addi.dk/2.34/',
-    openholdingstatus: 'https://openholdingstatus.addi.dk/3.0/',
+    ddbcmsapi: 'https://cmscontent.dbc.dk/',
+    moreinfo: 'https://moreinfo.addi.dk/2.10/',
+    openagency: 'http://vipcore.iscrum-vip-prod.svc.cloud.dbc.dk/1.0/api',
+    openholdingstatus: 'https://openholdingstatus.addi.dk/2.2/',
     PRODopenorder: 'https://openorder.addi.dk/3.0',
-    openorder: 'https://openorder.addi.dk/3.0',
-    opensearch: 'http://opensearch.addi.dk/b3.5_5.0/',
+    openorder: 'https://openorder-tst.addi.dk/3.0',
+    opensearch: 'https://opensearch.addi.dk/b3.5_5.0/',
     openuserstatus: 'https://openuserstatus.addi.dk/1.6.1/',
-    rank: 'https://xptest.dbc.dk/ms/rank/v1',
-    suggestpopular: 'XXXXX',
-    suggestcreator: 'XXXXX',
-    suggestlibrary: 'XXXXX',
-    suggestsubject: 'XXXXX',
-    recommendurls: 'XXXXX'
+    suggest: 'http://ortograf.mcp1-proxy.dbc.dk/ortograf/',
+    recommend: 'http://booklens-190101-1-0.mi-prod.svc.cloud.dbc.dk/',
+    performance: 'http://elk/elasticsearch',
+    communityservice: 'http://localhost:4010/v1',
+    cicero: 'https://cicero-fbs.com/rest/external/v1/',
+    openformat:
+      'http://openformat-php-develop.frontend-features.svc.cloud.dbc.dk/server.php',
+    holdingsitems:
+      'http://holdings-items-content-service.cisterne.svc.cloud.dbc.dk/holdings-items-content-service/api',
+    infomediaservice: 'http://infomedia.mcp1-proxy.dbc.dk/server.php'
   },
-  search: {agency: '775100', profile: 'opac'},
-  netpunkt: {user: 'XXXXX', group: 'XXXXX', password: 'XXXXX'},
+  cicero: { 'DK-710100': 'XXXXX' },
+  performance: { username: 'XXXXX', password: 'XXXXX' },
+  communityservice: { id: 1 },
+  search: {
+    agency: '775100',
+    profile: 'opac',
+    collectionidentifiers: ''
+  },
+  storage: { user: 'XXXXX' },
+  netpunkt: { user: 'XXXXX', group: 'XXXXX', password: 'XXXXX' },
   user: {
-    agency: '100450',
-    libraryId: '100450',
-    isil: 'DK-100450',
-    id: 'XXXXX',
+    id: 'C04122017433',
+    salt: 'XXXXX',
     pin: 'XXXXX',
-    salt: 'XXXXX'
+    libraryId: '790900',
+    agency: '790900',
+    isil: 'DK-790900'
   },
   app: {
-    clientid: 'XXXXX',
+    clientId: 'XXXXX',
     ddbcmsapipassword: 'XXXXX',
     orderpolicyrequester: '190101',
     orderSystem: 'bibliotekdk',
-    ips: ['10.10.10.0'],
-    access_token: 'qwerty'
+    ips: { '0': 'XXXXX' },
+    access_token: 'XXXXX'
   }
 };
 const mockData = {
-  '["openorder","<SOAP-ENV:Envelope xmlns=\\"http://oss.dbc.dk/ns/openorder\\" xmlns:SOAP-ENV=\\"http://schemas.xmlsoap.org/soap/envelope/\\">\\n     <SOAP-ENV:Body>\\n        <placeOrderRequest>\\n           <authentication>\\n              <groupIdAut>XXXXX</groupIdAut>\\n              <passwordAut>XXXXX</passwordAut>\\n              <userIdAut>XXXXX</userIdAut>\\n           </authentication>\\n           <copy>false</copy>\\n           <exactEdition>false</exactEdition>\\n           <needBeforeDate>2020-12-01T00:00:00</needBeforeDate>\\n           <orderSystem>bibliotekdk</orderSystem>\\n           <pickUpAgencyId>DK-100450</pickUpAgencyId>\\n           <pid>870970-basis:28126727</pid>\\n           <pid>870970-basis:27597726</pid>\\n           <serviceRequester>190101</serviceRequester>\\n           <userAddress>ADDRESS</userAddress>\\n           <userId>XXXXX</userId>\\n           <userIdAuthenticated>true</userIdAuthenticated>\\n           <userMail>EMAIL</userMail>\\n           \\n           <userTelephone>123454</userTelephone>\\n           <verificationReferenceSource>dbcdatawell</verificationReferenceSource>\\n         </placeOrderRequest>\\n      </SOAP-ENV:Body>\\n    </SOAP-ENV:Envelope>"]':
-    '<?xml version=\'1.0\' encoding=\'UTF-8\'?><S:Envelope xmlns:S="http://schemas.xmlsoap.org/soap/envelope/"><S:Body><ns1:placeOrderResponse xmlns:ns1="http://oss.dbc.dk/ns/openorder"><ns1:orderPlaced><ns1:orderId>1033955652</ns1:orderId><ns1:orderPlacedMessage>owned_accepted</ns1:orderPlacedMessage></ns1:orderPlaced></ns1:placeOrderResponse></S:Body></S:Envelope>'
+  '["openorder","<SOAP-ENV:Envelope xmlns=\\"http://oss.dbc.dk/ns/openorder\\" xmlns:SOAP-ENV=\\"http://schemas.xmlsoap.org/soap/envelope/\\">\\n     <SOAP-ENV:Body>\\n        <placeOrderRequest>\\n           <authentication>\\n              <groupIdAut>XXXXX</groupIdAut>\\n              <passwordAut>XXXXX</passwordAut>\\n              <userIdAut>XXXXX</userIdAut>\\n           </authentication>\\n           <copy>false</copy>\\n           <exactEdition>false</exactEdition>\\n           <needBeforeDate>2021-06-01T00:00:00</needBeforeDate>\\n           <orderSystem>bibliotekdk</orderSystem>\\n           <pickUpAgencyId>DK-790900</pickUpAgencyId>\\n           <pid>870970-basis:28126727</pid>\\n           <pid>870970-basis:27597726</pid>\\n           <serviceRequester>190101</serviceRequester>\\n           \\n           <userId>C04122017433</userId>\\n           <userIdAuthenticated>true</userIdAuthenticated>\\n           \\n           \\n           \\n           <verificationReferenceSource>dbcdatawell</verificationReferenceSource>\\n         </placeOrderRequest>\\n      </SOAP-ENV:Body>\\n    </SOAP-ENV:Envelope>"]':
+    '<?xml version=\'1.0\' encoding=\'UTF-8\'?><S:Envelope xmlns:S="http://schemas.xmlsoap.org/soap/envelope/"><S:Body><ns1:placeOrderResponse xmlns:ns1="http://oss.dbc.dk/ns/openorder"><ns1:orderPlaced><ns1:orderId>1039377316</ns1:orderId><ns1:orderPlacedMessage>not_owned_ILL_loc</ns1:orderPlacedMessage></ns1:orderPlaced></ns1:placeOrderResponse></S:Body></S:Envelope>'
 };
 
 import Provider from '../../provider/Provider.js';
-import {assert, fail} from 'chai';
+import { assert, fail } from 'chai';
+
 const provider = Provider();
 
 describe('Automated test: order_multiple_pids_ordered.snapshot', () => {
   it('has same result as recorded (in order_multiple_pids_ordered.snapshot)', () => {
     assert(
-      Date.now() < +new Date('2021-01-01'),
+      Date.now() < +new Date('2021-03-08'),
       'Please recreate the automatically generated unit tests, such that the mock data does not come out of sync with the actual services. See README.md for details.'
     );
     context.mockData = mockData;
