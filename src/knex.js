fd01a70fb451886e5845609ec179e6e85b9cb201
pjo <pjo@dbc.dk>
2021-10-11 13:38:24 +0200

    Revert "removed sqlite3 + reference"
    This reverts commit 524c65c06cdaa75d70b0f2da0e560db61ec10f30.

--- src/knex.js
+++ src/knex.js
@@ -1,7 +1,14 @@
-const config = {
-  client: 'pg',
-  connection: process.env.PG_CONNECTION_STRING
-};
+const config = process.env.PG_CONNECTION_STRING
+  ? {
+      client: 'pg',
+      connection: process.env.PG_CONNECTION_STRING
+    }
+  : {
+      client: 'sqlite3',
+      connection: {
+        filename: `${__dirname}/../serviceprovider.sqlite3`
+      }
+    };
 
 const knex = require('knex')(config);
 module.exports = {knex};
