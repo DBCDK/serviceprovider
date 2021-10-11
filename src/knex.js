524c65c06cdaa75d70b0f2da0e560db61ec10f30
pjo <pjo@dbc.dk>
2021-10-11 13:23:48 +0200

    removed sqlite3 + reference

--- src/knex.js
+++ src/knex.js
@@ -1,14 +1,7 @@
-const config = process.env.PG_CONNECTION_STRING
-  ? {
-      client: 'pg',
-      connection: process.env.PG_CONNECTION_STRING
-    }
-  : {
-      client: 'sqlite3',
-      connection: {
-        filename: `${__dirname}/../serviceprovider.sqlite3`
-      }
-    };
+const config = {
+  client: 'pg',
+  connection: process.env.PG_CONNECTION_STRING
+};
 
 const knex = require('knex')(config);
 module.exports = {knex};
